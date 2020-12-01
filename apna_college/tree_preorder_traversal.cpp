cbcccc40b45a8af52911b53110c94de53d015f33
shyamal2411 <sgprajapati24@gmail.com>
2020-12-01 17:04:39 +0530

    Tree PreOrder Traversal

---
+++ apna_college/tree_preorder_traversal.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node {
+  struct node* left;
+  int data;
+  struct node* right;
+};
+
+struct node* createnode(int val) {
+  struct node* temp = (struct node*)malloc(sizeof(struct node));
+  temp->data = val;
+  temp->right = NULL;
+  temp->left = NULL;
+  // temp->left=temp->right=NULL;
+};
+void preorder(struct node* root) {
+  if (root != NULL) {
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+  }
+}
+struct node* insertnode(struct node* node, int val) {
+  if (node == NULL)
+    return createnode(val);
+
+  if (val < node->data)
+    node->left = insertnode(node->left, val);
+
+  else if (val > node->data)
+    node->right = insertnode(node->right, val);
+
+  return node;
+}
+
+int main() {
+  struct node* root = NULL;
+  root = insertnode(root, 4);
+  insertnode(root, 5);
+  insertnode(root, 1);
+  insertnode(root, 2);
+  insertnode(root, 3);
+  insertnode(root, 7);
+  insertnode(root, 9);
+  
+  cout << "Pre-Order traversal of the Binary Search Tree is: ";
+  preorder(root);
+  
+  return 0;
+}
