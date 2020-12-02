aaea5e5922f6841c2e060e3c77bad1dcbfb6dde2
shyamal2411 <sgprajapati24@gmail.com>
2020-12-02 22:41:41 +0530

    added tutorial solution

--- A_Catching_the_Impostor.cpp
+++ A_Catching_the_Impostor.cpp
@@ -26,4 +26,27 @@ int main() {
     cout << "YES" << endl;
   else
     cout << "NO" << endl;
+    return 0;
 }
+/*
+//! code from the tutorial-solution
+#include "bits/stdc++.h"
+using namespace std;
+int main()
+{
+    set<int> s;
+        int n, m;
+        cin >> n >> m;
+        for(int i = 0;i < m;i++){
+                int x;
+                cin >> x;
+                s.insert(x);
+        }
+        if(s.size() == n - 1){
+                cout << "YES";
+        }else{
+                cout << "NO";
+        }
+}
+*/
+
