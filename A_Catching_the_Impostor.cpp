9842a4867611ada5b44ab79b06d245ad5f964320
shyamal2411 <sgprajapati24@gmail.com>
2020-12-02 22:11:16 +0530

    codeforces gym catching the imposter code

---
+++ A_Catching_the_Impostor.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+#define ll long long
+#define fo(i, n) for (int i = 0; i < n; i++)
+
+int main() {
+  ll int n, k, x, t, i, p, q, r, s, count = 0, j;
+  cin >> n >> k;//number of players and size of list.
+  ll int arr[n];
+  fo(i, n) arr[i] = 0; //as said in tutorial- to initiate all elements to 0 for frequency
+
+  fo(i, k) {
+    cin >> x; //players
+    if (arr[x - 1] == 0)
+      arr[x - 1] = 1;
+  }
+  int ans = 0;
+  fo(i, n) {
+    if (arr[i])
+      ans++;
+  }
+  if (ans == n - 1)
+    cout << "YES" << endl;
+  else
+    cout << "NO" << endl;
+}
--- Tree_BinaryTreeCProgram.c
+++ Tree_BinaryTreeCProgram.c
@@ -2,95 +2,95 @@
 #include <stdlib.h>
 
 struct node {
-  struct node* lchild;
-  struct node* rchild;
-  int data;
+	struct node* lchild;
+	struct node* rchild;
+	int data;
 };
 // REFERENCE: AKBARI SIR:
 // https://www.learningcrux.com/video/mastering-data-structures-algorithms-using-c-andc/14/19
-struct qu {
-  int size;
-  int front;
-  int rear;
-  node** q;  // *q is for array and node * is for storing elements of type nodes
+struct node {
+	int size;
+	int front;
+	int rear;
+	node **q;  // *q is for array and node * is for storing elements of type nodes
 };
 
 void Treecreate(struct qu* q, int size) {
-  q->size = size;
-  q->front = q->rear = 0;
-  q->Q = (node**)malloc(q->size *
-                        sizeof(node*));  // array created dynamically from heap.
+	q->size = size;
+	q->front = q->rear = 0;
+	q->Q = (node**)malloc(q->size *
+	                      sizeof(node*));  // array created dynamically from heap.
 }
 
 void enqueue(struct qu* q, node* q) {
-  if ((q->rear + 1) % q->size == q->front)
-    printf("Queue is Full.");
-  else {
-    q->rear = (q->rear) % q->size;
-    q->Q[q->rear] = x;
-  }
+	if ((q->rear + 1) % q->size == q->front)
+		printf("Queue is Full.");
+	else {
+		q->rear = (q->rear) % q->size;
+		q->Q[q->rear] = x;
+	}
 }
 
 node* dequeue(struct qu* q) {
-  node* x = NULL;
-  if (q->front == q->rear)
-    printf("Queue is Empty.\n");
-  else {
-    q->front = (q->front + 1) % q->size;
-    x = q->Q[q->front];
-  }
-  return x;
+	node* x = NULL;
+	if (q->front == q->rear)
+		printf("Queue is Empty.\n");
+	else {
+		q->front = (q->front + 1) % q->size;
+		x = q->Q[q->front];
+	}
+	return x;
 }
 
 struct node* root = NULL;
 void create() {
-  struct node *p, *t;
-  int x;
-  struct queue q;
-  create(&q, 100);
-  printf("Enter Root Value: ");
-  scanf("%d", &x);
-  root = (struct node*)malloc(sizeof(struct node));
-  root->data = x;
-  root->lchild = root->rchild = NULL;
-  enqueue(&q, root);
+	struct node *p, *t;
+	int x;
+	struct queue q;
+	create(&q, 100);
+	printf("Enter Root Value: ");
+	scanf("%d", &x);
+	root = (struct node*)malloc(sizeof(struct node));
+	root->data = x;
+	root->lchild = root->rchild = NULL;
+	enqueue(&q, root);
 
-  while (!isempty(q)) {
-    p = dequeue(&q);
-    printf("Enter left child");
-    scanf("%d", &x);
-    if (x != -1) {
-      t = (struct node*)malloc(sizeof(struct node));
-      t->data = x;
-      t->lchild = t->rchild = NULL;  // leaf node.
-      p->lchild = t;
-      enqueue(&q, t);
-    }
-    printf("Enter right child");
-    scanf("%d", &x);
-    if (x != -1) {
-      t = (struct node*)malloc(sizeof(struct node));
-      t->data = x;
-      t->rchild = t->lchild = NULL;  // leaf node.
-      p->rchild = t;
-      enqueue(&q, t);
-    }
-  }
+	while (!isempty(q)) {
+		p = dequeue(&q);
+		printf("Enter left child");
+		scanf("%d", &x);
+		if (x != -1) {
+			t = (struct node*)malloc(sizeof(struct node));
+			t->data = x;
+			t->lchild = t->rchild = NULL;  // leaf node.
+			p->lchild = t;
+			enqueue(&q, t);
+		}
+		printf("Enter right child");
+		scanf("%d", &x);
+		if (x != -1) {
+			t = (struct node*)malloc(sizeof(struct node));
+			t->data = x;
+			t->rchild = t->lchild = NULL;  // leaf node.
+			p->rchild = t;
+			enqueue(&q, t);
+		}
+	}
 }
 
 int isempty(struct qu q) {
-  return q.front = q.rear;  // will return true or false.
+	return q.front = q.rear;  // will return true or false.
 }
 void preorder(struct node) {
-  if (p) {
-    printf("%d", p->data);
-    preorder(p->lchild);
-    preorder(p->rchild);
-  }
+	if (p) {
+		printf("%d", p->data);
+		preorder(p->lchild);
+		preorder(p->rchild);
+	}
 }
 int main() {
-  Treecreate();
-  preoder(root);
+	Treecreate();
+	preoder(root);
 
-  return 0;
+	return 0;
 }
