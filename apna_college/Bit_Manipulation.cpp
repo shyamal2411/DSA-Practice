663783783166b2d0aeae3a6f29f1cb6a3f88796a
shyamal2411 <sgprajapati24@gmail.com>
2020-12-03 11:24:23 +0530

    feat setbit

--- apna_college/Bit_Manipulation.cpp
+++ apna_college/Bit_Manipulation.cpp
@@ -11,9 +11,14 @@ int getbit(int n,int pos)
     return ((n & (1<<pos))!=0);
 }
 
+int setbit(int n,int pos)
+{
+    return (n | (1<<pos));
+}
 int main(){
     ll int n,k,t,i,p,q,r,s,count=0,j;
 
-cout<<getbit(5,2)<<endl;
+//cout<<getbit(5,2)<<endl;
+  cout<<setbit(5,1);
     return 0;   
 }
