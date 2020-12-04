424dc322bae36f1ac1b2b6cf84c4bea8efd7854b
shyamal2411 <sgprajapati24@gmail.com>
2020-12-04 19:22:04 +0530

    feat comments

--- Even_Pair_Sum.cpp
+++ Even_Pair_Sum.cpp
@@ -14,11 +14,12 @@ int main() {
   while (n--) {
     cin >> p >> q;
     ll int ans, p1, q1, p2, q2;
-    p1 = p / 2;
-    q1 = p - p1;
-    p2 = q / 2;
-    q2 = q - p2;
-    ans = p1 * p2 + q1 * q2;
+    p1 = p / 2;   // getting even numbers out of p
+    q1 = p - p1;  // odd numbers out of p
+    p2 = q / 2;   // getting even numbers out of q
+    q2 = q - p2;  // odd numbers out of q
+    ans = p1 * p2 + q1 * q2;  // sum of even numbers can be sum of odd+odd and
+                              // even+even but can never be odd+even
     cout << ans << "\n";
   }  // end of while
   return 0;
