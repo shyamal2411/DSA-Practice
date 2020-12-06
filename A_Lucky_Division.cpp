1d021d2e666a5574d2c53b2a138e8b6775b9f36a
Shyamal <sgprajapati24@gmail.com>
2020-12-06 23:49:30 +0530

    right approach

--- A_Lucky_Division.cpp
+++ A_Lucky_Division.cpp
@@ -2,14 +2,23 @@
 #include<string>
 #include<algorithm>
 using namespace std;
-
+//right approach, the previous approach didn't satisfy the Hidden test case no.8
 #define ll long long 
 #define fo(i,n) for(int i=0;i<n;i++)
 
 int main(){
     ll int n,k,t,i,p,q,r,s,count=0,j;
     cin>>n;
-    if(n%4==0 || n%7==0|| n%47==0)
+    
+    bool flag=0;
+    int arr[12]={4,7,47,74,44,444,777,477,774,474,447,744}; //didn't know this method, as not aware abt hidden test cases.
+   for(i=0;i<12;i++)
+   {
+       if(n%arr[i]==0){
+           flag=true;
+       }
+   }
+   if(flag)
     cout<<"YES";
 
     else
