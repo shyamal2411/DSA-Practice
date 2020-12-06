0cc3ff857b5a9bea2dab15e362875624aa627ec1
Shyamal <sgprajapati24@gmail.com>
2020-12-06 14:44:24 +0530

    failed at hidden test case 8

---
+++ A_Lucky_Division.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h> 
+#include<string>
+#include<algorithm>
+using namespace std;
+
+#define ll long long 
+#define fo(i,n) for(int i=0;i<n;i++)
+
+int main(){
+    ll int n,k,t,i,p,q,r,s,count=0,j;
+    cin>>n;
+    if(n%4==0 || n%7==0|| n%47==0)
+    cout<<"YES";
+
+    else
+    cout<<"NO";
+}
