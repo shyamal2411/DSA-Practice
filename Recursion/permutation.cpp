a1695abd0f565cdf0f5d18089838bee584187210
Shyamal <sgprajapati24@gmail.com>
2020-12-08 18:17:42 +0530

    permutation of given string

--- Recursion/permutation.cpp
+++ Recursion/permutation.cpp
@@ -9,10 +9,7 @@ void permutation(string s, string ans) {
   for (int i = 0; i < s.length(); i++) {
     char ch = s[i];
     string ros =
-        s.substr(0, i) +
-        s.substr(
-            i +
-            1);  // it means 0 is the starting point and i is the length.
+        s.substr(0, i) + s.substr(i +1);  // it means 0 is the starting point and i is the length.
                  // also i+1 denotes the starting point in the same operation.
     permutation(ros,ans + ch);
   }
