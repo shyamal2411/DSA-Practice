f46e713c4dbf833265df885f6102103d0469fc54
Shyamal <sgprajapati24@gmail.com>
2020-12-10 11:29:25 +0530

    basics of pairs

---
+++ pair_basics.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h> 
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+
+int main(){
+   int n,i,t,count=0,j;
+ pair <int,char> p;
+ p.first=3;
+ p.second='f';
+     
+return 0;
+}
