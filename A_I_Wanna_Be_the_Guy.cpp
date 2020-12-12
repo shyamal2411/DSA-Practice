4725eaa6e641265efd32365f9cbcab3fb2a7a0be
Shyamal <sgprajapati24@gmail.com>
2020-12-12 12:12:24 +0530

    Codeforces 469A

--- A_I_Wanna_Be_the_Guy.cpp
+++ A_I_Wanna_Be_the_Guy.cpp
@@ -8,21 +8,24 @@ int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
   ll int n, k, t, i, p, q, r, s, count = 0, j;
+  set<int> w;
   cin >> n;
-  int a[p], b[q];
-
   cin >> p;
-  fo(i, p) { cin >> a[i]; }
-  cout << endl;
-  cin >> q;
-  fo(i, q) { cin >> b[i]; }
-  cout << endl;
 
-  fo(i, n) {
-    if (a[i] || b[i] == n)
-      cout << "I become the guy." << endl;
-    else
-      cout << "Oh, my keyboard!" << endl;
+  while (p--) {
+    cin >> r;
+    w.insert(r);
   }
+
+  cin >> q;
+  while (q--) {
+    cin >> k;
+    w.insert(k);
+  }
+  if (w.size() == n)
+    cout << "I become the guy." << endl;
+  else
+    cout << "Oh, my keyboard!" << endl;
+
   return 0;
 }
