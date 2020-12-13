e1fd37744cf3abddde0b3094320c5fa92816133a
Shyamal <sgprajapati24@gmail.com>
2020-12-13 17:16:40 +0530

    Codeforces 281 A

---
+++ A_Word_Capitalization.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fo(i, n) for (int i = 0; i < n; i++)
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  ll int n, k, t, i, p, q, r, s, count = 0, j;
+
+  string str;
+  cin >> str;
+  str[0] = towupper(str[0]);
+  cout << str << endl;
+  //! REMEMBER NOT TO CONVERT THE REST OF THE STRING TO LOWER CASE, AS PER THE
+  //! FIRST TEST CASE
+  //! THIS FUNCTION CAN BE USED TO TRANSFORM THE WHOLE STRING TO UPPER OR LOWER
+  //! CASE
+  //*    transform(sl.begin(), sl.end(), sl.begin(), ::tolower);
+  return 0;
+}
