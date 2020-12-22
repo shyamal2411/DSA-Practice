a5bcfc9c5e133045ce9e4b2f2e319f64e3999e47
Shyamal <sgprajapati24@gmail.com>
2020-12-22 11:00:27 +0530

    Gate C programs practice'
    q

---
+++ Gate_roughwork.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+/*
+int main()
+{
+    printf("%d",printf("Hi!")*printf("Bye"));
+    return 0;
+}*/
+//Ouput: Hi!Bye9
+//because printf("Hi!") will return 3
+//printf("Bye") will return 3
+// as there's * in between,so 3*3=9
+
+/*int main()
+{
+    int m=10,n=20;
+    printf("%d %d %d",m ,n,mn);
+    return 0;
+}*/
+//output:compile time error, because there's nothing like mn.
+
