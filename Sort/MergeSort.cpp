d27f90b21f6910d9660f6f65bf69c110172dd08c
Shyamal <sgprajapati24@gmail.com>
2020-12-26 00:26:48 +0530

    Added Explanation

--- Sort/MergeSort.cpp
+++ Sort/MergeSort.cpp
@@ -14,24 +14,24 @@ void merge(int arr[], int l, int mid, int r) {
   for (int i = 0; i < m; i++) {
     b[i] = arr[mid + l + i];
   }
-  int i=0;
-  int j=0;
-  int k=l;
-  while (i<n&&j<m)
-  {
-      if(a[i]<b[j]){
-          arr[k]=a[i];
-          k++; i++;
-      }
-      else{
-          arr[k]=b[j];
-          k++; j++;
-      }
+  int i = 0;
+  int j = 0;
+  int k = l;
+  while (i < n && j < m) {
+    if (a[i] < b[j]) {
+      arr[k] = a[i];
+      k++;
+      i++;
+    } else {
+      arr[k] = b[j];
+      k++;
+      j++;
+    }
   }
-  while(i<n)
-  {
-      arr[k]=a[i];
-      k++; i++;
+  while (i < n) {
+    arr[k] = a[i];
+    k++;
+    i++;
   }
   while (i < m) {
     arr[k] = b[j];
@@ -50,12 +50,19 @@ void mergesort(int arr[], int l, int r) {
 }
 
 int main() {
+  int arr[] = {5, 4, 3, 2, 1};
+  mergesort(arr, 0, 4);
+  for (int i = 0; i < 5; i++)
+    cout << arr[i] << " ";
 
-    int arr[]={5,4,3,2,1};
-    mergesort(arr,0,4);
-    for(int i=0;i<5;i++)
-    cout<<arr[i]<<" ";
-
-    cout<<endl;
-    return 0;
+  cout << endl;
+  return 0;
 }
+
+/*First of all divide the array into 2 equal halves
+  Then recursively go on dividing the elements into 2 parts
+  then sort those 2 elements mutually,
+  doing this for both the 2 major halves,we get 2 sorted arrays
+  afterwards go on comparing respective elements of both the halved arrays
+  and add it to the one single array.
+*/
