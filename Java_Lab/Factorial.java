ebbfe4618692ff323995726072502779725a4dc1
Shyamal <sgprajapati24@gmail.com>
2021-01-22 11:36:13 +0530

    Factorial in java

---
+++ Java_Lab/Factorial.java
@@ -0,0 +1,11 @@
+package Java_Lab;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int n = 6, factorial = 1;
+        for (int a = 1; a <= n; a++) {
+            factorial = factorial * a;
+        }
+        System.out.println("Factorial of " + n + " is : " + factorial);
+    }
+}
