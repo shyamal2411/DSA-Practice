1aed2a575f5f6a0177f01a5ef7d0e4a42f988700
Shyamal <sgprajapati24@gmail.com>
2021-01-23 21:30:48 +0530

    random changes

--- bhagwan questions/q1.c
+++ bhagwan questions/q1.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define fo(i,n) for(int i=0;i<n;i++)
 
 int main() {
-#ifndef ONLINE_JUDGE
-  freopen("input.txt", "r", stdin);
-  freopen("output.txt", "w", stdout);
-#endif
 
   int n;
   scanf("%d", &n);
   int a[n];
   fo(i, n) { scanf("%d", &a[n]); }
-  fo(i, n) { printf("%d", a[n]); }
+  fo(i, n) { printf("%d\t", a[n]); }
 }
