729bd5e8ae932712c8dbb4e643b63945dbb9e321
Shyamal <sgprajapati24@gmail.com>
2021-01-25 18:00:13 +0530

    Better readable code

--- bhagwan questions/q5.cpp
+++ bhagwan questions/q5.cpp
@@ -1,42 +1,38 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define fo(i,n) for(i=0;i<n;i++)
 #define ll long long
+#define fo(i, n) for (int i = 0; i < n; i++)
 
-void rearrange(int arr[], int n)
-{
-	int j = 0;
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] < 0)
-		{
-			if (i != j)
-				swap(arr[i], arr[j]);
-			j++;
-		}
-	}
+void rearrange(int arr[], int n) {
+  int j = 0;
+  for (int i = 0; i < n; i++) {
+    if (arr[i] < 0) {
+      if (i != j)
+        swap(arr[i], arr[j]);
+      j++;
+    }
+  }
+}
+void getarr(int arr[], int n) {
+  fo(i, n) cin >> arr[i];
+}
+void printarr(int arr[], int n) {
+  fo(i, n) cout << arr[i] << " ";
 }
 
-int main()
-{
+int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   ll int n, i, p, q, count = 0, j, t, temp;
-
   cin >> n;
   int arr[n];
-  for (i = 0; i < n; i++)
-    cin >> arr[i];
+  getarr(arr, n);
   rearrange(arr, n);
-
-  for (i = 0; i < n; i++)
-    cout << arr[i] << " ";
+  printarr(arr, n);
 }
 
 /*Input:
 5
 -1 5 -6 9 2
-
 Output: -6 -1 2 5 9
 */
