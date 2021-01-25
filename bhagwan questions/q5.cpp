2ca5c99103e60a6a62a5e70bb203a5b23f4eefe7
Shyamal <sgprajapati24@gmail.com>
2021-01-25 17:37:24 +0530

    Rearrange -ve ele

--- bhagwan questions/q5.cpp
+++ bhagwan questions/q5.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-//#define fo(i,n) for(i=0;i<n;i++)
+#define fo(i,n) for(i=0;i<n;i++)
 #define ll long long
-/*THIS CODE WORKS PERFECTLY ON ONLINE COMPILER! THERE'S SOME PROBLEM WITH SUBLIME.*/
+
 void rearrange(int arr[], int n)
 {
 	int j = 0;
@@ -20,20 +20,18 @@ void rearrange(int arr[], int n)
 
 int main()
 {
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif
-	ll int n, i, p, q, count = 0, j, t, temp;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll int n, i, p, q, count = 0, j, t, temp;
 
-	cin >> n;
-	int arr[n];
-	for (i = 0; i < n; i++)
-		cin >> arr[i];
-	rearrange(arr, n);
+  cin >> n;
+  int arr[n];
+  for (i = 0; i < n; i++)
+    cin >> arr[i];
+  rearrange(arr, n);
 
-	for (i = 0; i < n; i++)
-		cout << arr[i] << " ";
+  for (i = 0; i < n; i++)
+    cout << arr[i] << " ";
 }
 
 /*Input:
