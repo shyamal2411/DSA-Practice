4465f9512fbb9f96ec89be317209ba2bce0b7971
Shyamal <sgprajapati24@gmail.com>
2021-02-05 23:17:06 +0530

    inheritance program

---
+++ Java_Lab/Hospital.java
@@ -0,0 +1,21 @@
+package Java_Lab;
+
+class Doctor {
+    void DoctorDetails() {
+        System.out.println("Doctor Details...");
+    }
+}
+
+class Surgeon extends Doctor {
+    void SurgeonDetails() {
+        System.out.println("Surgen Detail...");
+    }
+}
+
+public class Hospital {
+    public static void main(String args[]) {
+        Surgeon s = new Surgeon();
+        s.DoctorDetails();
+        s.SurgeonDetails();
+    }
+}
