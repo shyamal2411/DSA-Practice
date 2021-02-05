806c3c027079da42ec4a9debc5467eb9bfc5e125
Shyamal <sgprajapati24@gmail.com>
2021-02-05 23:21:15 +0530

    Calculator error

---
+++ Java_Lab/Calculator.java
@@ -0,0 +1,29 @@
+package Java_Lab;
+class Calculate {
+   int z;
+	
+   public void add(int x, int y) {
+      z = x + y;
+      System.out.println("The sum of the given numbers:"+z);
+   }
+	
+   public void Sub(int x, int y) {
+      z = x - y;
+      System.out.println("The difference between the given numbers:"+z);
+   }
+}
+
+public class My_Calculation extends Calculate {
+   public void multi(int x, int y) {
+      z = x * y;
+      System.out.println("The product of the given numbers:"+z);
+   }
+	
+   public static void main(String args[]) {
+      int a = 20, b = 10;
+      My_Calculation demo = new My_Calculation();
+      demo.add(a, b);
+      demo.Sub(a, b);
+      demo.multi(a, b);
+   }
+}
