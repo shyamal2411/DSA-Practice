fe55ca45414408cfe45fb48767fcbe190cac655e
Shyamal <sgprajapati24@gmail.com>
2021-02-05 23:13:10 +0530

    init

---
+++ Java_Lab/inheritance.java
@@ -0,0 +1,21 @@
+package Java_Lab;
+
+class Animal {
+    void eat() {
+        System.out.println("eating...");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("barking...");
+    }
+}
+
+class inheritance {
+    public static void main(String args[]) {
+        Dog d = new Dog();
+        d.bark();
+        d.eat();
+    }
+}
