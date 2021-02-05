88718c1fe09bc4722e3ca9681d54efa925bf2a56
Shyamal <sgprajapati24@gmail.com>
2021-02-05 17:32:51 +0530

    CC Feb. Long A 2021

---
+++ Highest_Divisor.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+    ll int n,k,t,i,p,q,r,s,count=0,j;
+        cin>>n;
+
+    for(i=10;i>0;i--)
+    {
+        if(!(n%i))
+       { cout<<i<<endl;
+        break;
+       }
+    }
+return 0;
+}
