1b01c084c05624c5250911bf05b825244251116c
Shyamal <sgprajapati24@gmail.com>
2021-02-13 13:41:48 +0530

    cf div.2

---
+++ A_Add_&_Divide.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+    ll int a,b,n,k,t,i,p,q,r,s,count=0,j;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b;
+        while(a)
+        {
+            a=           
+        }
+    }
+return 0;
+}
