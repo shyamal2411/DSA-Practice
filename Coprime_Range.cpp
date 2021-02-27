2bf80f601b4b225b7edc86f4a4f05f8d52fcc8cc
Shyamal <sgprajapati24@gmail.com>
2021-02-27 22:20:51 +0530

    Accepted

---
+++ Coprime_Range.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+    ll int n,k,t,i,p,q,r,s,count=0,j;
+ cin>>t;
+ while (t--)
+ {
+   ll int x, y;
+   cin >> x >> y;
+
+   cout << 100000007 << "\n";
+ }
+    
+return 0;
+}
