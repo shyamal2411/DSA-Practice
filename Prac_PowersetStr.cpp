4b1876fc29a1dcde55f86e49f6d135052833cba5
Shyamal <sgprajapati24@gmail.com>
2021-03-22 19:26:21 +0530

    Practice-1

---
+++ Prac_PowersetStr.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void powerset(string str, int index = -1, string curr = "") {
+  int n = str.length();
+  if (index == n)
+    return;  // base cases
+
+  cout << curr << endl;
+
+  for (int i = index + 1; i < n; i++) {
+    curr += str[i];
+    powerset(str, i, curr);
+
+    curr.erase(curr.size() - 1);
+  }
+  return;
+}
+
+int main() {
+  string str;
+  cin >> str;
+  powerset(str);
+  return 0;
+}
