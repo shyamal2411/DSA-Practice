9b70f76a2662ad7e05f0a901e0b839f4b2dbe0f2
Shyamal <sgprajapati24@gmail.com>
2021-03-22 17:39:23 +0530

    Recursion in String
    Added comments

--- PalindromeStrRecursion.cpp
+++ PalindromeStrRecursion.cpp
@@ -5,10 +5,10 @@ bool isPalindrome(string s, int l, int r) {
   if (l >= r)  // > for even length of string, = for odd length of string.
     return true;
 
-  if (s[l] != s[r])
+  if (s[l] != s[r]) // if characters doesn't match from either side, then simply return false.
     return false;
 
-  return isPalindrome(s, l + 1, r - 1);
+  return isPalindrome(s, l + 1, r - 1);// we go on comparing characters from both the sides
 }
 
 int main() {
