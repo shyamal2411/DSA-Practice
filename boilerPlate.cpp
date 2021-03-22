8ca93f613f4e04f0512329daacf93c212898d530
Shyamal <sgprajapati24@gmail.com>
2021-03-22 11:36:09 +0530

    Template code

---
+++ boilerPlate.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+#define int long long
+#define f(i, a, n) for (int i = a; i < n; i++)
+#define rf(i, a, n) for (int i = n - 1; i >= a; i--)
+#define read(arr, n)          \
+  for (int i = 0; i < n; i++) \
+    cin >> arr[i];
+#define pr(arr, n)            \
+  for (int i = 0; i < n; i++) \
+    cout << arr[i];
+#define in cin >>
+#define out cout <<
+
+void solve() {
+  int n, k, t, i, p, q, r, s, count = 0, j, flag;
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t;
+  t = 1;
+  // in t;
+  while (t--) {
+    solve();
+  }
+}
