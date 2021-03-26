785ec54703dd5be27f4e56c5aa6830a781b056e0
Shyamal <sgprajapati24@gmail.com>
2021-03-26 17:35:06 +0530

    Practice Program

---
+++ Java_Lab/StaticNonStatic.java
@@ -0,0 +1,22 @@
+package Java_Lab;
+
+public class StaticNonStatic {
+    // Static method
+    static void myStaticMethod() {
+        System.out.println("Static Class");
+    }
+
+    // Public method
+    public void myPublicMethod() {
+        System.out.println("Public objects");
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        myStaticMethod(); // Call the static method
+        // myPublicMethod(); This would compile an error
+
+        StaticNonStatic myObj = new StaticNonStatic(); // Create an object of Main
+        myObj.myPublicMethod(); // Call the public method on the object
+    }
+}
