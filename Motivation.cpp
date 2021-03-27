0cb1bc104ea4e3964f9d5d557dc181b35fa33488
Shyamal <sgprajapati24@gmail.com>
2021-03-27 22:15:01 +0530

    CC LunchMarch A

---
+++ Motivation.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+#define int long long
+#define f(i, a, n) for (int i = a; i < n; i++)
+#define rf(i, a, n) for (int i = n - 1; i >= a; i--)
+#define read(arr, n)          \
+  for (int i = 0; i < n; i++) \
+    cin >> arr[i];
+#define pr(arr, n)            \
+  for (int i = 0; i < n; i++) \
+    cout << arr[i];
+#define in cin >>
+#define out cout <<
+
+void solve() {
+  int k, t, i, j, p, q, count = 0, flag;
+  int n, m, c = 0, x, d = 0, z = 0;
+  cin >> n >> x;
+
+  f(i, 0, n) {
+    cin >> m >> d;
+    if (m <= x && d > z)
+      z = d;
+  }
+
+  cout << z << endl;
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t;
+  t = 1;
+  in t;
+  while (t--) {
+    solve();
+  }
+}
