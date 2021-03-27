5503274a007681e0f8f71a5de3263a13bb91f447
Shyamal <sgprajapati24@gmail.com>
2021-03-27 16:40:03 +0530

    Sort example
    Sorting string to confirm concept

---
+++ StringSort.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+void solve() {
+  int n, k, t, i, p, q, r, s, count = 0, j, flag;
+  string str;
+  str = "cdab";
+  sort(str.begin(), str.end());
+  cout << str << endl;
+}
+
+int32_t main() {
+  int t;
+  t = 1;
+  // in t;
+  while (t--) {
+    solve();
+  }
+}
