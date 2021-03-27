7f4cdb8dad10fe63072dd583fff83754864726f3
Shyamal <sgprajapati24@gmail.com>
2021-03-27 16:25:12 +0530

    CF 141 A

---
+++ A_Amusing_Joke.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+#define int long long
+#define f(i, a, n) for (int i = a; i < n; i++)
+#define rf(i, a, n) for (int i = n - 1; i >= a; i--)
+#define read(arr, n)          \
+  for (int i = 0; i < n; i++) \
+    cin >> arr[i];
+#define pr(arr, n)            \
+  for (int i = 0; i < n; i++) \
+    cout << arr[i];
+#define in cin >>
+#define out cout <<
+
+void solve() {
+  int n, k, t, i, p, q, r, s, count = 0, j, flag;
+  string sa, sb, sc, sd;
+  cin >> sa >> sb >> sc;
+  sd += sa + sb;
+
+  sort(sd.begin(), sd.end());
+  sort(sc.begin(), sc.end());
+
+  if (sc == sd)
+    cout << "YES";
+
+  else
+    cout << "NO";
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t;
+  t = 1;
+  // in t;
+  while (t--) {
+    solve();
+  }
+}
