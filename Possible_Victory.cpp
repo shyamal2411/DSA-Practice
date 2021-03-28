cd728868b62a8f97c51d36f0af9e693479675a78
Shyamal <sgprajapati24@gmail.com>
2021-03-28 23:23:50 +0530

    CC Starter 1

---
+++ Possible_Victory.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+#define int long long
+#define f(i, a, n) for (int i = a; i < n; i++)
+#define rf(i, a, n) for (int i = n - 1; i >= a; i--)
+#define read(arr, n)          \
+  for (int i = 0; i < n; i++) \
+    cin >> arr[i];
+#define pr(arr, n)            \
+  for (int i = 0; i < n; i++) \
+    cout << arr[i];
+#define in cin >>
+#define out cout <<
+
+void solve() {
+  int ans = 0, o, c, n, k, t, i, p, q, r, s, count = 0, j, flag;
+
+  cin >> r >> o >> c;
+  count = 20 - o;
+  ans = count * 6 * 6;
+  ans+=c;
+  if (ans > r)
+    cout << "YES" << endl;
+
+  else
+    cout << "NO" << endl;
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t;
+  t = 1;
+  // in t;
+  while (t--) {
+    solve();
+  }
+}
