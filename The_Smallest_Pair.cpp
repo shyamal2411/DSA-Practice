3edd6000183b8020d8af982324bbdb6fdc41951e
Shyamal <sgprajapati24@gmail.com>
2021-03-31 16:41:10 +0530

    Vector Approach

--- The_Smallest_Pair.cpp
+++ The_Smallest_Pair.cpp
@@ -36,3 +36,13 @@ int32_t main(){
         solve();  
     }    
 }
+
+/*
+VECTOR:
+cin >> n;
+vector<int> v(n);
+for(int i=0;i<n;i++)
+cin >> v[i];
+sort(v.begin(),v.end());
+cout << v[0]+v[1] << endl;
+*/
