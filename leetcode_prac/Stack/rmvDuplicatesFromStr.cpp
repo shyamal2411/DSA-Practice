c698968d8a36d4d4cf159dc4d4bcbf50a38589d3
Shyamal <sgprajapati24@gmail.com>
2021-04-12 15:07:06 +0530

    rmv duplicates from Str through stack

---
+++ leetcode_prac/Stack/rmvDuplicatesFromStr.cpp
@@ -0,0 +1,12 @@
+string removeDuplicates(string s)
+
+{
+    int i=0, n=s.length();
+    for(int j=0; j < n; j++,i++)
+    {
+        s[i] = s[j];
+        if(i>0 && s[i-1] ==s[i])
+        i-=2;
+    }
+    return s.substr(0,i);
+}
