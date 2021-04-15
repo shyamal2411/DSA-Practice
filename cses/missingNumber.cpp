cfbb3fa7bb41f555dc717b9df2ad015a9ed58740
Shyamal <sgprajapati24@gmail.com>
2021-04-15 13:12:52 +0530

    Optimal alternative

--- cses/missingNumber.cpp
+++ cses/missingNumber.cpp
@@ -20,24 +20,35 @@ using namespace std;
 #define second sc
 
 void solve() {
-  int n, k, t, i, p, q, r, count = 0, j, flag;
-  cin >> n;
-  int arr[n - 1];
-  int f[200001] = {0};// taking this huge number as code failed on this number!!!
-  for (int i = 0; i < n - 1; i++) {
-    cin >> arr[i];
-    f[arr[i]]++;
+  int n, s = 0;
+  in n;  // 5
+  for (int i = 1; i < n; i++) {
+    int a;
+    in a;    // 1 2 3 5
+    s += a;  // 1->3->6->11
   }
-
-  sort(arr, arr + n);
-
-  for (int i = 1; i < 200001; i++) {
-    if (f[i] == 0) {
-      cout << i;
-      break;
-    }
-  }
+  cout << n * (n + 1) / 2 - s;  // 5*6/2-11=30/2-11==15-11=4==ans.
 }
+
+// void solve() {
+//   int n, k, t, i, p, q, r, count = 0, j, flag;
+//   cin >> n;
+//   int arr[n - 1];
+//   int f[200001] = {0};// taking this huge number as code failed on this
+//   number!!! for (int i = 0; i < n - 1; i++) {
+//     cin >> arr[i];
+//     f[arr[i]]++;
+//   }
+
+//   sort(arr, arr + n);
+
+//   for (int i = 1; i < 200001; i++) {
+//     if (f[i] == 0) {
+//       cout << i;
+//       break;
+//     }
+//   }
+// }
 
 int32_t main() {
   ios_base::sync_with_stdio(false);
