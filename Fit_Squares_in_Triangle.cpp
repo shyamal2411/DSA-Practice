7cf31b3375862cb0b5ea284093e7925e668526ef
Shyamal <sgprajapati24@gmail.com>
2021-04-19 15:48:33 +0530

    Implementation

---
+++ Fit_Squares_in_Triangle.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define int long long
+#define nl endl
+#define f(i, a, n) for (int i = a; i < n; i++)
+#define rf(i, a, n) for (int i = n - 1; i >= a; i--)
+#define read(arr, n)          \
+  for (int i = 0; i < n; i++) \
+    cin >> arr[i];
+#define pr(arr, n)            \
+  for (int i = 0; i < n; i++) \
+    cout << arr[i];
+#define in cin >>
+#define out cout <<
+#define first fr
+#define second sc
+
+void solve() {
+  int n = 0, b, k, i, p, q, r, count = 0, j, flag, ans;
+  cin >> b;
+  while (b > 2) {
+    n = n + (b - 2) / 2;
+    b = b - 2;
+  }
+  cout << n << endl;
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t;
+  t = 1;
+  in t;
+  while (t--) {
+    solve();
+  }
+}
