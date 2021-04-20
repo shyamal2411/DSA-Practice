3a6f613d89d8066137de3cc98e9e7963297c8343
Shyamal <sgprajapati24@gmail.com>
2021-04-20 08:50:48 +0530

    CF Div.2 716 B

---
+++ B_AND_0_Sum_Big.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define int long long
+#define nl endl
+#define f(i, a, n) for (int i = a; i < n; i++)
+#define rf(i, a, n) for (int i = n - 1; i >= a; i--)
+#define read(arr, n)          \
+  for (int i = 0; i < n; i++) \
+    cin >> arr[i];
+#define pr(arr, n)            \
+  for (int i = 0; i < n; i++) \
+    cout << arr[i];
+#define in cin >>
+#define out cout <<
+#define first fr
+#define second sc
+const int m = 1e9 + 7;
+void solve() {
+  int n, k, i, p, q, r, count = 0, j, flag, ans;
+  in n >> k;
+  ans = 1;
+  f(i, 1, k + 1) { ans = (ans * n) % m; }
+  out ans % m << nl;
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t;
+  t = 1;
+  in t;
+  while (t--) {
+    solve();
+  }
+}
