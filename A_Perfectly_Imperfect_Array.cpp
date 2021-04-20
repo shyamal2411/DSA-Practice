abebae0b272f82c2f204ad3fd18945ce881dbc38
Shyamal <sgprajapati24@gmail.com>
2021-04-20 08:47:29 +0530

    CF Div.2 716 A

---
+++ A_Perfectly_Imperfect_Array.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define int long long
+#define nl endl
+#define f(i, a, n) for (int i = a; i < n; i++)
+#define rf(i, a, n) for (int i = n - 1; i >= a; i--)
+#define read(arr, n)          \
+  for (int i = 0; i < n; i++) \
+    cin >> arr[i];
+#define pr(arr, n)            \
+  for (int i = 0; i < n; i++) \
+    cout << arr[i];
+#define in cin >>
+#define out cout <<
+#define first fr
+#define second sc
+
+void solve() {
+  int n, k, i, p, q, r, count = 0, j, flag, ans;
+  cin >> n;
+
+  vector<int> v(n);
+  read(v, n);
+
+  for (auto itr : v) {
+    ans = sqrt(itr);
+
+    if (ans * ans != itr) {
+      cout << "YES\n";
+      return;
+    }
+  }
+  cout << "NO\n";
+}
+//just check if the element is perfect square,print yes else no.
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t;
+  t = 1;
+  in t;
+  while (t--) {
+    solve();
+  }
+}
