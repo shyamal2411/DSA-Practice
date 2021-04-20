0ff00a8a33e05bddc1238b3b7c43cd068e052394
Shyamal <sgprajapati24@gmail.com>
2021-04-20 18:17:27 +0530

    easy

---
+++ Total_Expenses.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define int long long
+#define nl endl
+#define f(i, a, n) for (int i = a; i < n; i++)
+#define rf(i, a, n) for (int i = n - 1; i >= a; i--)
+#define read(arr, n)          \
+  for (int i = 0; i < n; i++) \
+    cin >> arr[i];
+#define pr(arr, n)            \
+  for (int i = 0; i < n; i++) \
+    cout << arr[i];
+#define in cin >>
+#define out cout <<
+#define first fr
+#define second sc
+
+void solve() {
+  int n, k, i, p, q, r, count = 0, j, flag;
+  int quant, price;
+  double ans, total;
+  cin >> quant >> price;
+  ans = quant * price;
+  if (quant > 1000) {
+    total = ans - (ans * 10) / 100;
+    cout << fixed << setprecision(6) << total
+         << endl;  // for setting float precision.
+  } else {
+    cout << fixed << setprecision(6) << ans << endl;
+  }
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t;
+  t = 1;
+  in t;
+  while (t--) {
+    solve();
+  }
+}
