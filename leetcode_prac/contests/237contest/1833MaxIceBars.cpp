5be60714e68e9b2cf515e6fe283ceefb4153674b
Shyamal <sgprajapati24@gmail.com>
2021-04-26 22:12:12 +0530

    LT 237weekly B

---
+++ leetcode_prac/contests/237contest/1833MaxIceBars.cpp
@@ -0,0 +1,16 @@
+class Solution {
+ public:
+  int maxIceCream(vector<int>& costs, int coins) {
+    sort(costs.begin(),
+         costs.end());  // start buying from cheapest & buy until
+                        // you have coins jingling in your pocket.
+    for (int i = 0; i < costs.size(); i++) {
+      if (coins >= costs[i])  // if you've coins> the the price, the decrease
+                              // the coins by the cost.
+        coins -= costs[i];
+      else
+        return i;  // else return the index where we stopped.
+    }
+    return costs.size();
+  }
+};
