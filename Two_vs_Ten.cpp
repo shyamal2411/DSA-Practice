863128d2d4a9cb5abec22762a3b69fe5e0047f4b
Shyamal <sgprajapati24@gmail.com>
2021-04-27 22:31:54 +0530

    schl prblm

---
+++ Two_vs_Ten.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h> 
+#include<string>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+#define int long long 
+#define nl endl
+const int mod=1000000007;
+typedef vector<int> vi;
+#define pi pair<int,int>
+#define f(i,a,n) for(int i=a;i<n;i++)
+#define rf(i,a,n) for(int i=n-1;i>=a;i--)
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cin.tie(0);cout.tie(0);
+#define read(arr,n) for(int i=0;i<n;i++)cin>>arr[i];
+#define pr(arr,n) for(int i=0;i<n;i++)cout<<arr[i];
+#define in cin>>
+#define out cout<<
+#define first fr
+#define second sc
+
+void solve(){
+int x,n,k,i,p,q,r,count=0,j,flag,ans=0;
+ in x;
+    if(x%10==0)
+    out 0<<nl;
+
+    else if(x%5==0)
+    out 1<<nl;
+
+    else
+    out -1<<nl;
+}
+
+
+int32_t main(){
+    FIO int t; t=1;
+    in t;
+    while(t--) solve();
+  
+return 0;
+}
