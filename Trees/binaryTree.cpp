9b4ef13ec965be9376b49461c86bb1c1bc79afc4
Shyamal <sgprajapati24@gmail.com>
2021-04-30 23:29:43 +0530

    iterative traversal

--- Trees/binaryTree.cpp
+++ Trees/binaryTree.cpp
@@ -27,7 +27,7 @@ void preorder(Node* root) {
     cout << (root->key) << " ";
     preorder(root->left);
     preorder(root->right);
-  }
+  }  // Time complexity: O(n)
 }
 
 void postorder(Node* root) {
@@ -35,7 +35,7 @@ void postorder(Node* root) {
     postorder(root->left);
     postorder(root->right);
     cout << (root->key) << " ";
-  }
+  }  // Time complexity: O(n)
 }
 
 int getSize(Node* root) {
@@ -65,6 +65,67 @@ int height(Node *root)
     else
     return max(height(root->left),height(root->right))+1;
 }
+
+void iterative_Inorder(Node *root) //unable to understand the working of the code. 
+{
+  stack<Node*> st;
+  Node *curr=root;//initially current is at root.
+  while(curr!=NULL||st.empty()==false)
+  {
+    while(curr!=NULL)
+    {
+      st.push(curr);
+      curr=curr->left;
+    }
+    curr=st.top(); st.pop();
+    cout<<(curr->key)<<" ";
+    curr=curr->right;
+  }
+}
+
+
+void iterative_Preorder(Node *root)
+{
+  if(root==NULL) return;
+
+  stack<Node*> st;
+  st.push(root);
+  while(st.empty()==false)
+  {
+    Node *curr=st.top();
+    cout<<(curr->key)<<" ";
+    st.pop();
+
+    if(curr->right!=NULL)
+    st.push(curr->right);
+    
+    if(curr->left!=NULL)
+    st.push(curr->left);
+  }
+} 
+
+//Optimal solution for preorder traversal.
+void iterative_Preorder_Opt(Node *root)
+{
+if(root==NULL) return;
+
+stack<Node*> st;
+Node *curr=root;
+
+while(curr!=NULL || st.empty()==false)
+{
+  while(curr!=NULL)
+  {
+    cout<<(curr->key)<<" ";
+    if(curr->right!=NULL)
+    st.push(curr->right);
+  }
+  curr=curr->left;
+  if(st.empty()==false)
+  {curr=st.top(); st.pop();}
+}
+}
+
 
 int main() {
   Node* root = new Node(1);
