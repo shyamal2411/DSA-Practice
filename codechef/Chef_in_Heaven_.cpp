23807fc0efc6cc0821f48f983a8c64a85d046ee9
Shyamal <sgprajapati24@gmail.com>
2021-04-30 23:32:46 +0530

    CC April LT Div.2 B

---
+++ codechef/Chef_in_Heaven_.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define nl endl
+const int mod = 1000000007;
+typedef vector<int> vi;
+#define pi pair<int, int>
+#define f(i, a, n) for (int i = a; i < n; i++)
+#define rf(i, a, n) for (int i = n - 1; i >= a; i--)
+#define FIO                         \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define read(arr, n)          \
+  for (int i = 0; i < n; i++) \
+    cin >> arr[i];
+#define pr(arr, n)            \
+  for (int i = 0; i < n; i++) \
+    cout << arr[i];
+#define in cin >>
+#define out cout <<
+#define first fr
+#define second sc
+
+void solve() {
+  int n, k, l, i, p, q, r, count = 0, j, flag, ans = 0;
+  in l;
+
+  string s;
+  cin >> s;
+
+  f(i, 0, s.size()) {
+    if (s[i] == '1') {
+      count++;
+    }
+
+    ans++;
+    if (2 * count >= ans) {
+      out "YES" << nl;
+      return;
+    }
+  }
+  if (2 * count >= ans) {
+    out "YES" << nl;
+    return;
+  }
+  out "NO" << nl;
+}
+
+int32_t main() {
+  FIO int t;
+  t = 1;
+  in t;
+  while (t--)
+    solve();
+
+  return 0;
+}
