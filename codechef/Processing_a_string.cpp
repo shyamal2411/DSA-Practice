4f5d3efdf16db8583c732281ee419d755b884dd6
Shyamal <sgprajapati24@gmail.com>
2021-05-01 23:48:27 +0530

    practice easy

---
+++ codechef/Processing_a_string.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+#define int long long 
+#define nl endl
+const int M=1e9+7;
+typedef vector<int> vi;
+#define pi pair<int,int>
+#define f(i,a,n) for(int i=a;i<n;i++)
+#define rf(i,a,n) for(int i=n-1;i>=a;i--)
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cin.tie(0);cout.tie(0);
+#define read(arr,n) for(int i=0;i<n;i++)cin>>arr[i];
+#define pr(arr,n) for(int i=0;i<n;i++)cout<<arr[i];
+#define in cin>>
+#define out cout<<
+#define first fr
+#define second sc
+
+void solve(){
+int n,k,i,p,q,r,count=0,j,flag,ans=0;
+ string s;
+ in s;
+ f(i,0,s.length())
+ {
+   if ('0'<=s[i] && '9'>=s[i])
+    count+=s[i]-'0';
+ }
+out count<<nl;
+}
+
+
+int32_t main(){
+    FIO int t; t=1;
+    in t;
+    while(t--) solve();
+  
+return 0;
+}
