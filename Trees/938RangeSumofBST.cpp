8e06bad860e2fba90391b4511996f9f2bcff5fb1
Shyamal <sgprajapati24@gmail.com>
2021-05-10 18:10:48 +0530

    BST : LT
    Sum of nodes between a range

---
+++ Trees/938RangeSumofBST.cpp
@@ -0,0 +1,17 @@
+class Solution {
+ public:
+  int rangeSumBST(TreeNode* root, int low, int high) {
+    if (!root)
+      return 0;
+
+    if (root->val >= low && root->val <= high) 
+      return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+    
+     else if (root->val < low) 
+      return rangeSumBST(root->right, low, high);
+     
+     else 
+      return rangeSumBST(root->left, low, high);
+    
+  }
+};
