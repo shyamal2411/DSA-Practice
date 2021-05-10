104fb7a9152f8d1df8ade42c5ae6a512bd251fba
Shyamal <sgprajapati24@gmail.com>
2021-05-10 18:12:23 +0530

    MinHeight of BST

---
+++ Trees/minHeightofTree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+ public:
+  int minDepth(TreeNode* root) {
+    if (root == NULL)
+      return 0;
+
+    if (!root->left)
+      return 1 + minDepth(root->right);
+    if (!root->right)
+      return 1 + minDepth(root->left);
+
+    return 1 + min(minDepth(root->left), minDepth(root->right));
+  }
+};
