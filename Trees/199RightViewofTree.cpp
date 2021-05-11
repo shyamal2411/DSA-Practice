45e7f935670bea164440f12bd7fc21332d1214d4
Shyamal <sgprajapati24@gmail.com>
2021-05-11 23:18:54 +0530

    BT: Right View of Tree

---
+++ Trees/199RightViewofTree.cpp
@@ -0,0 +1,24 @@
+class Solution {
+ public:
+  vector<int> rightSideView(TreeNode* root) {
+    queue<TreeNode*> q;
+    vector<int> vec;  // create vector for storing values of nodes
+
+    if (!root)
+      return vec;
+
+    q.push(root);  // first push root into the queue.
+    while (!q.empty()) {
+      vec.push_back(q.back()->val);  // start pushing values from queue to vec
+      for (int i = q.size(); i > 0; i--) {
+        TreeNode* temp = q.front();
+        q.pop();
+        if (temp->left)
+          q.push(temp->left);
+        if (temp->right)
+          q.push(temp->right);
+      }
+    }
+    return vec;
+  }
+}
