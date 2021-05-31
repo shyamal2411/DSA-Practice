953ca9492f6cee9f2f79dd9b24a23e0c9c5c1669
Shyamal <sgprajapati24@gmail.com>
2021-06-01 00:11:23 +0530

    triplets

---
+++ apna_college/TripletsVector.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+typedef vector<int> vi;
+#define f(i,a,n) for(int i=a;i<n;i++)
+#define read(arr,n) for(int i=0;i<n;i++)cin>>arr[i];
+
+vector<vi> triplets(vi arr, int targetSum) {
+  int n = arr.size();
+  sort(arr.begin(), arr.end());
+  vector<vi> result;
+
+  for (int i = 0; i <= n - 3; i++) {
+    int j = i + 1;
+    int k = n - 1;
+    while (j < k) {
+      int current_sum = arr[i];
+      current_sum += arr[i];
+      current_sum += arr[k];
+
+      if (current_sum == targetSum){
+          result.push_back({arr[i],arr[j],arr[k]});
+          j++; k--;
+      }
+      else if(current_sum > targetSum){k--;}
+      else{j++;}
+    }
+  }
+  return result;
+}
+
+int main() {
+  vi arr{1, 2, 3, 4, 5, 6, 7, 8, 9, 15};
+  int s = 18;
+  auto result = triplets(arr, s);
+  for(auto itr: result){
+      for(auto n: itr){
+          cout<<n<<" ";
+      }
+    cout<<"\n";
+  }
+  return 0;
+}
