9f96f306d7d8689bec45353eb0bd86f638068114
Shyamal <sgprajapati24@gmail.com>
2021-06-03 00:01:48 +0530

    Boundary Path sum in BT

---
+++ leetcode_prac/Tree/112PathSum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+ public:
+  bool hasPathSum(TreeNode* root, int targetSum) {
+    if (!root)
+      return false;
+    targetSum -= root->val; /*go on decrementing the sum if it doesn't get 0,
+                               then simply false.*/
+    if (targetSum == 0 and !root->left and !root->right)
+      return true;
+
+    return hasPathSum(root->left, targetSum) ||
+           hasPathSum(root->right, targetSum);
+    // if either of the sides then true else false
+  }
+};
