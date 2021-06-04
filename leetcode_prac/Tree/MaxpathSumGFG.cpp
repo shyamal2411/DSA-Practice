293d829b99a978f9e4cfa87a655ae5098bc10f08
Shyamal <sgprajapati24@gmail.com>
2021-06-04 16:29:58 +0530

    Max path sum: GFG

---
+++ leetcode_prac/Tree/MaxpathSumGFG.cpp
@@ -0,0 +1,22 @@
+class Solution {
+ public:
+  vector<int> check(Node* root) {
+    if (!root)
+      return {0, 0};
+    vector<int> a = check(root->left);
+    vector<int> b = check(root->right);
+
+    if (a[0] > b[0])
+      return {a[0] + 1, a[1] + root->data};
+
+    if (a[0] < b[0])
+      return {b[0] + 1, b[1] + root->data};
+
+    else return {a[0] + 1, max(a[1], b[1]) + root->data};
+  }
+
+  int sumOfLongRootToLeafPath(Node* root) {
+    vector<int> ans = check(root);
+    return ans[1];
+  }
+};
