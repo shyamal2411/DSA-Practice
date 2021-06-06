bbc467ae40d3d0998640bfee052d4c35878f39e4
Shyamal <sgprajapati24@gmail.com>
2021-06-06 23:48:49 +0530

    Remove Duplicate from Sorted Arr

---
+++ leetcode_prac/Arrays/26RemoveDuplifromSorted.cpp
@@ -0,0 +1,20 @@
+class Solution {
+ public:
+  int removeDuplicates(vector<int>& nums) {
+    if (nums.size() == 0)
+      return 0;  // corner case
+
+    int i = 0;
+
+    for (int j = 1; j < nums.size();
+         j++) {  // as there is hidden testcase 1, we loop from 1 & not 0
+      if (nums[j] != nums[i])  // if element isn't duplicating with previous one
+                               // then increment i
+      {
+        i++;
+        nums[i] = nums[j];  // initialize value of element at j with i
+      }
+    }
+    return i + 1;  // this length is the actual length of sorted & not duplicate
+  }
+};
