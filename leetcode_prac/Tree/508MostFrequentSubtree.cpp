25eb6ecf961e9fa066f82f7685643786bbc9a598
Shyamal <sgprajapati24@gmail.com>
2021-06-08 00:41:17 +0530

    explanation

--- leetcode_prac/Tree/508MostFrequentSubtree.cpp
+++ leetcode_prac/Tree/508MostFrequentSubtree.cpp
@@ -5,15 +5,14 @@ class Solution {
     if (!root) 
       return 0;
     
-
     int sum = util_recursive(root->left, count, maxcount) 
             + root->val +
               util_recursive(root->right, count, maxcount);
-    ++count[sum];
-    if (maxcount < count[sum]) {
-      maxcount = count[sum];
+      count[sum]++; /*we simply add the sum value into the count map container as a record.*/
+    if (maxcount < count[sum]) {   
+      maxcount = count[sum]; /*if the count at certain index gets greater than max value, initialize max with that count index. */
     }
-    return sum;
+    return sum; //return that max index
   }
 
   vector<int> findFrequentTreeSum(TreeNode* root) {
@@ -24,10 +23,15 @@ class Solution {
 
     vector<int> ans;
     for (const auto& p : count) {
-      if (p.second == maxcount) {
-        ans.push_back(p.first);
+        if (p.second == maxcount) {
+        ans.push_back(p.first); //p.second means the sum as returned from util, if that == max value, push its value to vector
       }
     }
-    return ans;
+    return ans;//return that vector.
   }
 };
+
+/*we're using maps because we have to keep the count that 
+whether there're 2 diff subtrees with same sum are present or not.
+Set can't be used as it doesn't store duplicates, but in map, we can check it.
+p.second of map is useful for maxcount of subtree */
