8e8ac84000d1ff60aa950c0ecc55587941a1cd25
Shyamal <sgprajapati24@gmail.com>
2021-06-09 23:35:04 +0530

    temp

--- Recursion/first.cpp
+++ Recursion/first.cpp
@@ -1,1 +1,11 @@
-afdasfdfas
+#include<bits/stdc++.h> 
+using namespace std;
+
+typedef vector<int> vi;
+#define f(i,a,n) for(int i=a;i<n;i++)
+#define read(arr,n) for(int i=0;i<n;i++)cin>>arr[i];
+
+int main(){
+
+return 0;
+}
