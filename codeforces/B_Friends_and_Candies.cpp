849e8fc73034a02138f324cf1ebc50ad7e0eb6c3
Shyamal <sgprajapati24@gmail.com>
2021-06-10 23:16:13 +0530

    CF contest 725 B

---
+++ codeforces/B_Friends_and_Candies.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define nl "\n"
+const int M = 1e9 + 7;
+typedef vector<int> vi;
+#define f(i, a, n) for (int i = a; i < n; i++)
+#define rf(i, a, n) for (int i = n - 1; i >= a; i--)
+#define FIO                         \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define read(arr, n)          \
+  for (int i = 0; i < n; i++) \
+    cin >> arr[i];
+#define pr(arr, n)            \
+  for (int i = 0; i < n; i++) \
+    cout << arr[i];
+#define in cin >>
+#define out cout <<
+#define first fr
+#define second sc
+
+void solve() {
+  int n, k, i, p, q, r, count = 0, j, flag, ans = 0, sum = 0;
+  in n;
+  int arr[n];
+
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+    sum += arr[i];
+  }
+
+  if (sum % n != 0) {
+    cout << -1 << nl;
+    return;
+  }
+
+  if (sum % n == 0) {
+    int temp = sum / n;
+
+    f(i, 0, n) {
+      if (arr[i] > temp)
+        ans++;
+    }
+  }
+  cout << ans << nl;
+}
+
+int32_t main() {
+  FIO int t;
+  t = 1;
+  in t;
+  while (t--)
+    solve();
+
+  return 0;
+}
