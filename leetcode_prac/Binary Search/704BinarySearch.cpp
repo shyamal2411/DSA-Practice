b1be95bd75be228596508c6244d1362538323438
Shyamal <sgprajapati24@gmail.com>
2021-06-10 23:15:00 +0530

    LC Binary search

---
+++ leetcode_prac/Binary Search/704BinarySearch.cpp
@@ -0,0 +1,17 @@
+class Solution {
+ public:
+  int search(vector<int>& nums, int target) {
+    int mid, low = 0, high = nums.size() - 1;
+    while (low <= high) {
+      mid = low + (high - low) / 2;
+
+      if (nums[mid] == target)
+        return mid;
+      else if (nums[mid] < target)
+        low = mid + 1;
+      else
+        high = mid - 1;
+    }
+    return -1;
+  }
+};
