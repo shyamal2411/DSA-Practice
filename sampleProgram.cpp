354e8072635e4f42b3e034db2b9b96cf03c3f1e5
SG <sgprajapati24@gmail.com>
2021-06-13 00:33:55 +0530

    Test Branch commit

---
+++ sampleProgram.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+typedef vector<int> vi;
+#define f(i,a,n) for(int i=a;i<n;i++)
+#define read(arr,n) for(int i=0;i<n;i++)cin>>arr[i];
+
+int main(){
+cout<<"Hey test branch"<<endl;
+return 0;
+}
