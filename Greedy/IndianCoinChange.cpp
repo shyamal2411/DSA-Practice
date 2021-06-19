08e341a61aa31a53453500d78830ee2cb2a82677
SG <sgprajapati24@gmail.com>
2021-06-19 23:30:21 +0530

    apna clg: Greedy algo

---
+++ Greedy/IndianCoinChange.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+// learned from: https://www.youtube.com/watch?v=IUQRh7SJwEM
+#define int long long
+#define nl "\n"
+const int M = 1e9 + 7;
+typedef vector<int> vi;
+#define f(i, a, n) for (int i = a; i < n; i++)
+#define rf(i, a, n) for (int i = n - 1; i >= a; i--)
+#define FIO                         \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define read(arr, n)          \
+  for (int i = 0; i < n; i++) \
+    cin >> arr[i];
+#define pr(arr, n)            \
+  for (int i = 0; i < n; i++) \
+    cout << arr[i];
+#define in cin >>
+#define out cout <<
+#define first fr
+#define second sc
+
+void solve() {
+  int n, k, i, p, q, r, count = 0, j, flag, ans = 0;
+  in n;
+  vi v(n);
+  read(v, n);
+  int x;
+  cin >> x;
+  sort(v.begin(), v.end(), greater<int>());
+
+  f(i, 0, n) {
+    ans += x / v[i];
+    x -= x / v[i] * v[i];
+  }
+  out ans << nl;
+}
+
+int32_t main() {
+  FIO int t;
+  t = 1;
+  // in t;
+  while (t--)
+    solve();
+
+  return 0;
+}
