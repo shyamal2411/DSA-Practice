6d351884e99de804bb004c443fb61759c584a3d6
SG <sgprajapati24@gmail.com>
2021-06-19 23:31:15 +0530

    CF Div.2 B upsolve

---
+++ B_Bad_Boy.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+#define int long long 
+#define nl "\n"
+const int M=1e9+7;
+typedef vector<int> vi;
+#define f(i,a,n) for(int i=a;i<n;i++)
+#define rf(i,a,n) for(int i=n-1;i>=a;i--)
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cin.tie(0);cout.tie(0);
+#define read(arr,n) for(int i=0;i<n;i++)cin>>arr[i];
+#define pr(arr,n) for(int i=0;i<n;i++)cout<<arr[i];
+#define in cin>>
+#define out cout<<
+#define first fr
+#define second sc
+
+void solve(){
+int n,k,i,p,q,r,count=0,j,flag,ans=0;
+ int m,x,y;
+ in n>>m>>x>>y;
+ cout<<n<<" "<<1<<" "<<1<<" "<<m<<nl;
+}
+
+void solve2(){
+    int n,m,k,l;
+    cin>>n>>m>>k>>l;
+    int x[]={1,n};
+    int y[]={1,m};
+    f(i,0,2)
+    {
+        f(j,0,2){
+            
+        }
+    }
+
+}
+
+int32_t main(){
+    FIO int t; t=1;
+    in t;
+    while(t--) solve2();
+  
+return 0;
+}
