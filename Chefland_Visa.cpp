fb38e1c83fb899206ccbac1e299e6c033a585d47
SG <sgprajapati24@gmail.com>
2021-06-20 23:54:12 +0530

    CC Cookoff June A

---
+++ Chefland_Visa.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define nl "\n"
+const int M = 1e9 + 7;
+typedef vector<int> vi;
+#define f(i, a, n) for (int i = a; i < n; i++)
+#define rf(i, a, n) for (int i = n - 1; i >= a; i--)
+#define FIO                         \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define read(arr, n)          \
+  for (int i = 0; i < n; i++) \
+    cin >> arr[i];
+#define pr(arr, n)            \
+  for (int i = 0; i < n; i++) \
+    cout << arr[i];
+#define in cin >>
+#define out cout <<
+#define first fr
+#define second sc
+
+void solve() {
+  int n, k, i, p, q, r, count = 0, j, flag, ans = 0;
+  int x1, x2, y1, y2, z1, z2;
+  in x1 >> x2 >> y1 >> y2 >> z1 >> z2;
+
+  if (x1 <= x2 && y1 <= y2 && z1 >= z2)
+    out "YES\n";
+  else
+    out "NO\n";
+}
+
+int32_t main() {
+  FIO int t;
+  t = 1;
+  in t;
+  while (t--)
+    solve();
+
+  return 0;
+}
