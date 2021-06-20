45ff80663e468a13bcf3a9fbfbf9c3141e61518c
SG <sgprajapati24@gmail.com>
2021-06-20 23:55:44 +0530

    Weekly 246: LC

---
+++ leetcode_prac/contests/1903LargestOddinStr.cpp
@@ -0,0 +1,20 @@
+class Solution {
+ public:
+  string largestOddNumber(string num) {
+    for (int i = num.size() - 1; i >= 0; i--) {
+      if ((num[i] - '0') % 2)
+        return num.substr(0, i + 1);
+    }
+    return string();
+  }
+};  // upsolved
+
+class Solution {
+ public:
+  string largestOddNumber(string num) {
+    return num.substr(0, num.find_last_of("13579") + 1);
+    // This function takes a given character and returns the position of the
+    // last occurrence of that character. it means, we'll find last of 13579 and
+    // add 1 to it.
+  }
+};  // upsolved
