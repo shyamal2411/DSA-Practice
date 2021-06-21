18309be2bc99e370908fc5c098198aa1b4a1c970
SG <sgprajapati24@gmail.com>
2021-06-21 23:57:55 +0530

    Added explanation

--- leetcode_prac/Hash Table/136SingleNumber.cpp
+++ leetcode_prac/Hash Table/136SingleNumber.cpp
@@ -1,4 +1,4 @@
-// didn't know bit manipulation, so couldn' solve.
+// didn't know bit manipulation, so couldn't solve.
 class Solution {
  public:
   int singleNumber(vector<int>& nums) {
@@ -8,3 +8,9 @@ class Solution {
     return ans;
   }
 };
+
+/*This XOR operation works because it's like XORing all the numbers by itself.
+So if the array is {2,1,4,5,2,4,1} then it will be like we are performing this
+operation
+
+((2^2)^(1^1)^(4^4)^(5)) => (0^0^0^5) => 5.*/
