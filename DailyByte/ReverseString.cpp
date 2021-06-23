225623fb9a0d7438e91373318254b5a098095291
SG <sgprajapati24@gmail.com>
2021-06-23 08:42:52 +0530

    reverse Str: DailyByte

---
+++ DailyByte/ReverseString.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define nl "\n"
+const int M = 1e9 + 7;
+typedef vector<int> vi;
+#define f(i, a, n) for (int i = a; i < n; i++)
+#define rf(i, a, n) for (int i = n - 1; i >= a; i--)
+#define FIO                         \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define read(arr, n)          \
+  for (int i = 0; i < n; i++) \
+    cin >> arr[i];
+#define pr(arr, n)            \
+  for (int i = 0; i < n; i++) \
+    cout << arr[i];
+#define in cin >>
+#define out cout <<
+#define first fr
+#define second sc
+
+string reverses(string s) {
+  string rev;
+  for (char i = s.size() - 1; i >= 0; i--) {
+    rev.push_back(s[i]);
+  }
+
+  return rev;
+}
+
+int32_t main() {
+  string s;
+  cin >> s;
+  cout << reverses(s);
+
+  return 0;
+}
