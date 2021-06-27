d0886aa45ac12d0491ca9533f371f1158e368ffd
SG <sgprajapati24@gmail.com>
2021-06-27 23:21:35 +0530

    Added prblm statement

--- DailyByte/ReverseString.cpp
+++ DailyByte/ReverseString.cpp
@@ -1,3 +1,13 @@
+/*
+This question is asked by Google. Given a string, reverse all of its characters
+and return the resulting string.
+
+Ex: Given the following strings...
+
+“Cat”, return “taC”
+“The Daily Byte”, return "etyB yliaD ehT”
+“civic”, return “civic”
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
