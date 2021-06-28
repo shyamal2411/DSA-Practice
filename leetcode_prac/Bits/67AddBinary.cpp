bcac453d7baadb63948225fa08d1581e861c57b5
SG <sgprajapati24@gmail.com>
2021-06-28 22:58:06 +0530

    LC: 67 Add Binary

---
+++ leetcode_prac/Bits/67AddBinary.cpp
@@ -0,0 +1,20 @@
+class Solution {
+ public:
+  string addBinary(string a, string b) {
+    string result = "";
+    int s = 0;
+
+    int i = a.size() - 1, j = b.size() - 1;
+    while (i >= 0 || j >= 0 || s == 1) {
+      s += ((i >= 0) ? a[i] - '0' : 0);
+      s += ((j >= 0) ? b[j] - '0' : 0);
+
+      result = char(s % 2 + '0') + result;
+      s /= 2;
+
+      i--;
+      j--;
+    }
+    return result;
+  }
+};  // notes
