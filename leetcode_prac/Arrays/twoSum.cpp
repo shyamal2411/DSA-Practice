b89ea2d298be7325d4c42cd19872b60d62ac610c
SG <sgprajapati24@gmail.com>
2021-06-29 23:01:12 +0530

    optimal approach O(n)

--- leetcode_prac/Arrays/twoSum.cpp
+++ leetcode_prac/Arrays/twoSum.cpp
@@ -12,6 +12,20 @@ class Solution {
     }
     return vec;
   }
+  // Optimized approach -used  unordered map to optimize
+  vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp;
+    // create map, find the element pair and return them
+    for (int i = 0; i < nums.size(); i++) {
+      if (mp.find(target - nums[i]) != mp.end()) {
+        return {mp[target - nums[i]], i};
+      }
+      mp[nums[i]] = i;  // update map
+    }
+    return {};
+  }
 };
+// previous approach was O(n^2) in time
+// this is O(n).
 
 /*https://leetcode.com/problems/two-sum */
