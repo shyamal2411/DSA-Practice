052a90f502c9b8e577361bc91502db0c8de00c84
SG <sgprajapati24@gmail.com>
2021-06-29 23:02:23 +0530

    LC: recursion

---
+++ leetcode_prac/Maths/342PowerofFour.cpp
@@ -0,0 +1,11 @@
+class Solution {
+ public:
+  bool isPowerOfFour(int n) {
+    if (n == 1)
+      return true;
+    if (n % 4 != 0 || n <= 0)
+      return false;
+
+    return isPowerOfFour(n / 4);
+  }
+};
