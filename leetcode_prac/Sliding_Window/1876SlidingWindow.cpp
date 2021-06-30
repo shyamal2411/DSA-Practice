861026568e887a3d5bf0e9e738ba1c5209ec208d
SG <sgprajapati24@gmail.com>
2021-06-30 23:18:50 +0530

    LC: 1876

--- leetcode_prac/Sliding_Window/1876SlidingWindow.cpp
+++ leetcode_prac/Sliding_Window/1876SlidingWindow.cpp
@@ -1,4 +1,14 @@
 class Solution {
  public:
-  int countGoodSubstrings(string s) {}
+  int countGoodSubstrings(string s) {
+    if (s.size() < 3)
+      return 0;
+
+    int i, count = 0, n = s.size();
+    for (i = 0; i < n - 2; i++)
+      if (s[i] != s[i + 1] && s[i] != s[i + 2] && s[i + 1] != s[i + 2])
+        count++;
+
+    return count;
+  }
 };
