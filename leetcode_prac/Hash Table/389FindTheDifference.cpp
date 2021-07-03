b5b4bc59c2a0313166133f9be3a4c2872f3a4b9f
SG <sgprajapati24@gmail.com>
2021-07-03 22:44:34 +0530

    LC: 389 revised code

--- leetcode_prac/Hash Table/389FindTheDifference.cpp
+++ leetcode_prac/Hash Table/389FindTheDifference.cpp
@@ -1,3 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define nl "\n"
+const int M = 1e9 + 7;
+typedef vector<int> vi;
+typedef vector<string> vs;
+#define f(i, a, n) for (int i = a; i < n; i++)
+#define FIO                         \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define read(arr, n)          \
+  for (int i = 0; i < n; i++) \
+    cin >> arr[i];
+#define pr(arr, n)            \
+  for (int i = 0; i < n; i++) \
+    cout << arr[i] << " ";
+#define in cin >>
+#define out cout <<
+
 class Solution {
  public:
   char findTheDifference(string s, string t) {
@@ -20,3 +43,27 @@ class Solution {
     return result;
   }
 };
+void solve() {
+  int n, k;
+  string s, t;
+  cin >> s >> t;
+  Solution ss;
+  //   ss.findTheDifference(s,t);
+  cout << ss.findTheDifference(s, t) << nl;
+}
+
+int32_t main() {
+  FIO int t;
+  t = 1;
+  //   in t;
+  while (t--)
+    solve();
+
+  return 0;
+}
+/*
+INPUT:
+abcd abcde
+OUTPUT:
+e
+*/
