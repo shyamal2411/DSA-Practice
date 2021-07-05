381da93701f2fdbddfa8af977e9de7ce8765704a
SG <sgprajapati24@gmail.com>
2021-07-05 21:14:43 +0530

    LC: 349 optimal solution

--- leetcode_prac/Hash Table/349Intersectionof2Arr.cpp
+++ leetcode_prac/Hash Table/349Intersectionof2Arr.cpp
@@ -9,3 +9,22 @@ public:
         return s;
     }
 }; //Stack overflow ¯\_(ツ)_/¯
+
+class Solution {
+ public:
+  vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    unordered_map<int, int> mp1, mp2;
+
+    for (auto itr : nums1)
+      mp1[itr]++;
+
+    vector<int> ans;
+    for (int i = 0; i < nums2.size(); i++) {
+      if (mp1.find(nums2[i]) != mp1.end() && mp2.find(nums2[i]) == mp2.end()) {
+        ans.push_back(nums2[i]);
+        mp2[nums2[i]]++;
+      }
+    }
+    return ans;
+  }
+};
