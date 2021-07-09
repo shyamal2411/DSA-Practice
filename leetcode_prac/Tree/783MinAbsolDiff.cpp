ea55d0cf0bf7058aec6c050fb9bde7a3f5af7083
SG <sgprajapati24@gmail.com>
2021-07-10 00:35:56 +0530

    LC: 783 Tree min abs diff

---
+++ leetcode_prac/Tree/783MinAbsolDiff.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+ public:
+  void help(TreeNode* root, int& pre, int& ans) {
+    if (root) {
+      help(root->left, pre, ans);
+      if (pre != -1)
+        ans = min(ans, root->val - pre);
+
+      pre = root->val;
+      help(root->right, pre, ans);
+    }
+  }
+
+  int minDiffInBST(TreeNode* root) {
+    int ans = INT_MAX, pre = -1;
+    help(root, pre, ans);
+    return ans;
+  }
+};
