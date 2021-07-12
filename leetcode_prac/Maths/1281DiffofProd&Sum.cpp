8c44aedf69c8ae0ab9fba7e60473b234d667884e
SG <sgprajapati24@gmail.com>
2021-07-12 22:03:42 +0530

    LC: 1281 Diff of Prod & Sum

---
+++ leetcode_prac/Maths/1281DiffofProd&Sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+ public:
+  int subtractProductAndSum(int n) {
+    int sum = 0, prod = 1;
+    while (n > 0) {
+      sum += n % 10;
+      prod *= n % 10;
+      n /= 10;
+    }
+    return prod - sum;
+  }
+};
