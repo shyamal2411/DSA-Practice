df8685aa83b068dfa5c8534b5aaf69609f4225bc
SG <sgprajapati24@gmail.com>
2021-07-14 23:47:19 +0530

    169: majority ele-array

---
+++ leetcode_prac/Arrays/169MajorityElement.cpp
@@ -0,0 +1,17 @@
+class Solution {
+ public:
+  int majorityElement(vector<int>& nums) {
+    int n = nums.size();
+    int count = 0, r;
+
+    for (int i = 0; i < n; i++) {
+      if (count == 0)
+        r = nums[i];
+      if (r == nums[i])
+        count++;
+      else
+        count--;
+    }
+    return r;
+  }
+};
