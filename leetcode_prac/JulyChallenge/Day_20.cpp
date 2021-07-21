2af8d8cf593df67710e59dbbf54550a62556959a
SG <sgprajapati24@gmail.com>
2021-07-21 23:32:25 +0530

    LC July challenge day-20

---
+++ leetcode_prac/JulyChallenge/Day_20.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    vector<int> original;
+    
+public:
+    Solution(vector<int>& nums) {
+            original=nums;
+    }
+    
+    vector<int> reset() {
+    return original;  
+    }
+    
+    vector<int> shuffle() {
+          vector<int> shuffled=original;
+        for(int i=original.size()-1;i>=0;i--){
+            int r=rand()%(i+1);
+            swap(shuffled[i], shuffled[r]);
+        }
+        return shuffled;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * vector<int> param_1 = obj->reset();
+ * vector<int> param_2 = obj->shuffle();
+ */
