f1143c90f47ba9ecbc9c58aafbf349e709c23b8e
SG <sgprajapati24@gmail.com>
2021-08-07 00:04:49 +0530

    Divide-Conquer: 493 reverse N pairs

---
+++ leetcode_prac/Divide&Conquer/493ReverseNPairs.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    
+    int count=0;
+    
+    void countHelp(vector<int>& nums,int start, int mid, int end){
+        
+        //2 pointers
+        int left=start, right=mid+1;
+        while(left<=mid && right<=end){
+            if((long) nums[left] > (long)2* nums[right]){
+                count+=(mid-left+1);
+                right++;
+            }
+            else
+            {left++;}
+        }
+    //Worst case: nlog(n)
+        sort(nums.begin()+start, nums.begin()+end+1);
+        return;
+        
+        //Sorting every step 
+    }
+    void mergeSort(vector<int>& nums, int start, int end){
+        if(start==end)
+            return;
+        int mid=(start+end)/2;
+        mergeSort(nums, start, mid);
+        mergeSort(nums, mid+1, end);
+        
+        countHelp(nums, start, mid, end);
+        return;
+    }
+    
+    int reversePairs(vector<int>& nums) {
+        if(!nums.size())
+            return 0;
+         count=0;
+        mergeSort(nums, 0, nums.size()-1);
+        return count;
+    }
+};
+int main(){
+    vector<int> nums={1,3,2,3,1};
+    Solution myclass;
+    int ret=myclass.reversePairs(nums);
+    cout<<ret<<endl;
+    return 0;
+}
