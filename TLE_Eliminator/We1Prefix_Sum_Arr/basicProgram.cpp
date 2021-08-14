3b9e7454ad9e82f92994716c8f669bf50fed9533
SG <sgprajapati24@gmail.com>
2021-08-14 23:53:35 +0530

    basic prefixSum idea

---
+++ TLE_Eliminator/We1Prefix_Sum_Arr/basicProgram.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const int n = 1e5 + 10;
+int a[n], pf[n];
+
+//! WITHOUT PREFIX SUM
+void regularManner() {
+  int n;
+  cin >> n;
+
+  for (int i = 1; i <= n; i++)
+    cin >> a[i];
+
+  int q;
+  cin >> q;
+  while (q--) {
+    int l, r;
+    cin >> l >> r;
+    int sum = 0;
+    for (int i = l; i <= r; i++) {
+      sum += a[i];
+    }
+    cout << sum << endl;
+  }
+}
+//! WITH PREFIX SUM
+void prefixSum() {
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+    pf[i] = pf[i - 1] + a[i];
+  }
+  int q;
+  cin >> q;
+  while (q--) {
+    int l, r;
+    cin >> l >> r;
+    cout << pf[r] - pf[l - 1] << endl;
+  }
+}
+
+int32_t main() {
+  int t;
+  t = 1;
+  // in t;
+  while (t--) {
+    regularManner();
+    // prefixSum();
+  }
+
+  return 0;
+}
+/*
+INPUT:
+6
+3 6 2 8 9 2
+4
+2 3
+4 6
+1 5
+3 6
+
+OUPUT:
+8
+19
+28
+21
+*/
