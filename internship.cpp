dc5be33af34c849e8c3a2162cf99bf620d8cfca4
Shyamal <sgprajapati24@gmail.com>
2021-09-03 21:01:16 +0530

    internship motivation

---
+++ internship.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int finally_internship, roz, savar, sanj, divas, raat, happiness;
+
+void mahenat(int roz) {}
+
+void mahenat(int savar, int sanj) {}
+void mahenat(int divas, int raat) {}
+
+int main()
+{
+  internship();
+}
+
+
+
+
+int internship()
+{
+  while (finally_internship == false)
+  {
+    mahenat(roz);
+    mahenat(savar, sanj);
+    mahenat(divas, raat);
+  }
+  return happiness;
+}
