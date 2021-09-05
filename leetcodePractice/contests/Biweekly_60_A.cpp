375d63fd8bbda40e880d897bcc316b0d21c680c5
Shyamal <sgprajapati24@gmail.com>
2021-09-05 09:56:21 +0530

    added solution

--- leetcodePractice/contests/Biweekly_60_A.cpp
+++ leetcodePractice/contests/Biweekly_60_A.cpp
@@ -19,6 +19,19 @@ public:
         }
         return -1;
     }
+int findMiddleIndex2(vector<int>& nums) 
+    {
+    int postfixsum=accumulate(nums.begin(),nums.end(),0); // calculate the sum of the array  
+    int prefixsum=0; // initially the prefix sum is =0;
+    for(int i=0;i<nums.size();i++)
+    {
+        postfixsum-=nums[i];//lets the mid element contender be i , subtract it from sum as it should neither be in prefixsum nor in postfixsum.
+        if(postfixsum==prefixsum)//check if prefixsum ==postfix sum  if yes return true.
+            return i;
+        prefixsum+=nums[i];//add the element int orpefix sum since the previous condition was false and this element is no longer the contender of middle index.
+    }
+        return -1;//if the above condition in the loop was false this pretty much concludes that we have no middle element in our array.
+    }
 };
 
 int main(){
