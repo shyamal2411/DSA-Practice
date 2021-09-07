3ea56481837ff76d332c8c7f48deb65cf73689ed
Shyamal <sgprajapati24@gmail.com>
2021-09-07 09:08:25 +0530

    hacerrank-1.2

---
+++ Hackerrank/CountTriplets.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+#define int long long 
+#define nl "\n"
+const int M=1e9+7;
+typedef vector<int> vi;
+#define f(i,a,n) for(int i=a;i<n;i++)
+#define rf(i,a,n) for(int i=n-1;i>=a;i--)
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cin.tie(0);cout.tie(0);
+#define read(arr,n) for(int i=0;i<n;i++)cin>>arr[i];
+#define pr(arr,n) for(int i=0;i<n;i++)cout<<arr[i];
+#define in cin>>
+#define out cout<<
+#define first fr
+#define second sc
+
+int arr[5], brr[5];
+
+ int32_t main() {
+    int ans1 = 0;
+    int ans2 = 0;
+    
+    for (int i = 0; i < 3; i++) {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < 3; i++) {
+        cin >> brr[i];
+
+        if (arr[i] < brr[i]) {
+            ans2 += 1;
+        } else if (arr[i] > brr[i]) {
+            ans1 += 1;
+        }
+    }
+
+    cout << ans1 << " " << ans2 <<endl;
+    return 0;
+}
+
