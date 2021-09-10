7127a1fa30af217ea9dcb4795b0e1610bad5b468
Shyamal <sgprajapati24@gmail.com>
2021-09-10 22:59:01 +0530

    446: DP new test cases solution

--- leetcodePractice/DP/446ArithmeticSubSeq2.cpp
+++ leetcodePractice/DP/446ArithmeticSubSeq2.cpp
@@ -33,9 +33,34 @@ public:
     }
 };
 
+//The above solution gives TLE for new test cases.
+class Solutionn {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        vector< unordered_map<long, long> > dp(nums.size());
+        int res = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                long diff = (long)nums[i] - (long)nums[j];
+                
+                dp[i][diff]++;
+                
+                if (dp[j].find(diff) != dp[j].end()) {
+                    dp[i][diff] += dp[j][diff];
+                    res += dp[j][diff];
+                }
+            }
+        }
+        
+        return res;
+    }
+};
+
+
 int main()
 {
-    Solution s;
+    Solutionn s;
     vector<int> nums{2,4,6,8,10};
     cout<<s.numberOfArithmeticSlices(nums);
     return 0;
