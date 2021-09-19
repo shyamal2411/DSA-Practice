e016cfca243f476f737064e7ab5ecae68395038d
Shyamal <sgprajapati24@gmail.com>
2021-09-20 00:07:17 +0530

    383: Ransom note: hash table

--- leetcodePractice/Hash Table/383RansomNote.cpp
+++ leetcodePractice/Hash Table/383RansomNote.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 // https://leetcode.com/problems/ransom-note/
+// https://leetcode.com/problems/ransom-note/discuss/1347638/C%2B%2B-Hashmap-Solution
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
@@ -12,9 +13,26 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> m;
+        for(int i=0;i<magazine.size();i++)
+            m[magazine[i]]++;
+        
+        for(int i=0;i<ransomNote.size();i++){
+            if(m[ransomNote[i]]==0)
+                return false;
+            else
+                m[ransomNote[i]]--;
+        }            
+        return true;
+    }
+};
+
 int main()
 {
-    Solution obj;
+    Solution2 obj;
     cout<<obj.canConstruct("aa","aa")<<endl;
     cout<<obj.canConstruct("aa","aab")<<endl;
     cout<<obj.canConstruct("aa","a")<<endl;
