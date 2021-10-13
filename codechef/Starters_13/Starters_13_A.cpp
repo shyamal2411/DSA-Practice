4e195b7a4723d7921fd17d7bb010dbc2585255f4
Shyamal <sgprajapati24@gmail.com>
2021-10-14 01:49:21 +0530

    CC starters 13-A upsolve

---
+++ codechef/Starters_13/Starters_13_A.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+// https://www.codechef.com/START13C/problems/SUPCHEF
+#define int long long 
+#define nl "\n"
+const int M=1e9+7;
+typedef vector<int> vi;
+#define f(i,a,n) for(int i=a;i<n;i++)
+#define rf(i,a,n) for(int i=n-1;i>=a;i--)
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cin.tie(0);cout.tie(0);
+#define read(arr,n) for(int i=0;i<n;i++)cin>>arr[i];
+#define pr(arr,n) for(int i=0;i<n;i++)cout<<arr[i];
+#define in cin>>
+#define out cout<<
+#define first fr
+#define second sc
+
+void solve(){
+int m,n,k,i,p,q,r,count=0,j,flag,ans=0;
+ cin>>m>>n>>k;
+ if((n*k)<m)
+ {
+     cout<<"YES\n";
+ }
+ else 
+ cout<<"NO\n";
+}
+
+
+int32_t main(){
+    FIO int t; t=1;
+    in t;
+    while(t--) solve();
+  
+return 0;
+}
