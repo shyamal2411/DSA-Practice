f03c7ac1d7f3e4fe76c7adc7af65ad7a53c0a230
Shyamal <sgprajapati24@gmail.com>
2021-10-19 18:43:02 +0530

    check bipartite practice

---
+++ leetcodePractice/Graph/CheckBipartitePrac.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool bipartite(int node, vector<int> adj[], int color[]){
+    if(color[node] == -1)
+    color[node]=1;
+
+    for(auto it: adj[node]){
+        if(color[it]==-1){
+            color[it] = -color[node];
+            if(!bipartite(it, adj,color)){
+                return false;
+            }
+            else if(color[it]==color[node]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+bool checkBipartite(vector<int> adj[], int n){
+    int color[n];
+    memset(color, -1, sizeof color);
+
+    for(int i=0;i<n;i++){
+        if(color[i]==-1)
+        {
+            if(!bipartite(i, adj, color)){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main(){
+    int n, m;
+    cin>>n>>m;
+    vector<int> adj[n];
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    if(checkBipartite(adj,n))
+    cout<<"YES\n";
+    else
+    cout<<"NO\n";
+}
