823b9dfcee3cb183c9783150220ee78daa3a18b1
Shyamal <sgprajapati24@gmail.com>
2021-10-22 22:41:06 +0530

    reverse Stack using recursion

--- bhagwan_question/Stack/reverseStackRecursion.cpp
+++ bhagwan_question/Stack/reverseStackRecursion.cpp
@@ -1,45 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// reverse Stack using recursion
 stack<char> st;
 string ns;
-//! DO AGAIN!
-char insertBottom(char x) {
-  if (st.size() == 0)
-    st.push(x);  // base condition
 
-  else {
-    char topelement = st.top();
+char insertBottom(char x)
+{
+  if (st.size() == 0)
+    st.push(x);
+  else
+  {
+
+    char a = st.top();
     st.pop();
     insertBottom(x);
-    st.push(topelement);
+    st.push(a);
   }
 }
 
-char reverse() {
-  if (st.size() > 0) {
+char reverse()
+{
+  if (st.size() > 0)
+  {
     char x = st.top();
     st.pop();
     reverse();
+
     insertBottom(x);
   }
 }
 
-int main() {
-  st.push('S');
-  st.push('H');
-  st.push('Y');
-  st.push('A');
-  st.push('M');
-  st.push('A');
-  st.push('L');
+
+int main()
+{
+  st.push('1');
+  st.push('2');
+  st.push('3');
+  st.push('4');
 
   reverse();
-  while (!st.empty()) {
+
+  while (!st.empty())
+  {
     char p = st.top();
     st.pop();
     ns += p;
   }
-  cout << ns[6] << " " << ns[5] << " " << ns[4] << " " << ns[3] << " " << ns[2]
-       << " " << ns[1] << " " << ns[0] << " ";
+  cout << ns[3] << " " << ns[2] << " " << ns[1] << " " << ns[0] << endl;
   return 0;
 }
