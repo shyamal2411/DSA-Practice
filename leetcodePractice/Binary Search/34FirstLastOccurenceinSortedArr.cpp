1cb9fb80377fffc744c92f5a505ea6f79da64113
Shyamal <sgprajapati24@gmail.com>
2021-11-03 18:06:43 +0530

    34: BS: 2nd approach

--- leetcodePractice/Binary Search/34FirstLastOccurenceinSortedArr.cpp
+++ leetcodePractice/Binary Search/34FirstLastOccurenceinSortedArr.cpp
@@ -18,8 +18,31 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int in =lower_bound(nums, target);
+        int im =lower_bound(nums, target+1)-1;
+        if(in<nums.size() && nums[in]==target)
+            return {in, im};
+        else
+            return {-1, -1};
+        }
+    int lower_bound(vector<int> &nums, int target){
+        int left=0, right=nums.size()-1;
+        while(left<=right){
+            int mid=left + (right-left)/2;
+            if(nums[mid]<target)
+                left=mid+1;
+            else
+                right=mid-1;
+        }
+        return left;
+    }
+};
+
 int main(){
-    Solution s;
+    Solution2 s;
     vector<int> nums{5,7,7,8,8,10};
     int target=8;
     vector<int> ans=s.searchRange(nums, target);
