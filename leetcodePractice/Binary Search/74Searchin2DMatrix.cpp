b93f5f60cf24d3ae3753b2aaf504b078e8188a2c
Shyamal <sgprajapati24@gmail.com>
2021-11-05 16:26:21 +0530

    Search in 2D Matrix

--- leetcodePractice/Binary Search/74Searchin2DMatrix.cpp
+++ leetcodePractice/Binary Search/74Searchin2DMatrix.cpp
@@ -28,6 +28,38 @@ public:
     }
 };
 
+class Solutionn {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if((matrix.size()==0) || (matrix[0].size()==0))
+            return false;
+        int row=0;
+        
+        while((row<matrix.size()) && (matrix[row].back()<target))
+            row++;
+        
+        if(row>=matrix.size())
+            return false;
+        
+        return bs(matrix[row], target);
+    }
+    
+    bool bs(vector<int> &matrix, int target){
+        int l=0, r=matrix.size();
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(matrix[mid]==target)
+                return true;
+            else if(matrix[mid]<target)
+                l=mid+1;
+            else
+                r=mid-1;
+        }
+        return false;
+    }
+};
+
 int main()
 {
     Solution s;
