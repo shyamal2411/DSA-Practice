b9c9a0e3fdcc14f15755162b240b1bab33eacacb
Shyamal <sgprajapati24@gmail.com>
2021-11-05 23:48:00 +0530

    CC Nov'21 Long B

---
+++ Equal_Coins.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//SHYAMAL PRAJAPATI
+// https://www.codechef.com/NOV21C/problems/EQUALCOIN
+#define int long long
+#define nl "\n"
+const int M = 1e9 + 7;
+typedef vector<int> vi;
+#define f(i, a, n) for (int i = a; i < n; i++)
+#define rf(i, a, n) for (int i = n - 1; i >= a; i--)
+#define FIO                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define read(arr, n)            \
+    for (int i = 0; i < n; i++) \
+        cin >> arr[i];
+#define pr(arr, n)              \
+    for (int i = 0; i < n; i++) \
+        cout << arr[i];
+#define in cin >>
+#define out cout <<
+#define first fr
+#define second sc
+
+void solve()
+{
+    int n, k, i, p, q, r, count = 0, j, flag, ans = 0;
+    int x, y;
+    cin >> x >> y;
+    ans = x + y;
+    if (x % 2 == 1)
+    {
+        cout << "NO\n";
+        return;
+    }
+    if (x % 2 == 0 && y % 2 == 0)
+    {
+        cout << "YES\n";
+        return;
+    }
+    if (x > 1)
+    {
+        cout << "YES\n";
+        return;
+    }
+    else
+    {
+        cout << "NO\n";
+        return;
+    }
+}
+
+int32_t main()
+{
+    FIO int t;
+    t = 1;
+    in t;
+    while (t--)
+        solve();
+
+    return 0;
+}
