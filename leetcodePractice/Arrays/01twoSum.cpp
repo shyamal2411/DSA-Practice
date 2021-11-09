41cc1f055f4050ded4717af6a1d10c43b534e86b
Shyamal <sgprajapati24@gmail.com>
2021-11-09 20:00:07 +0530

    two sum: Striver

--- leetcodePractice/Arrays/01twoSum.cpp
+++ leetcodePractice/Arrays/01twoSum.cpp
@@ -30,6 +30,23 @@ class Solution {
 // previous approach was O(n^2) in time
 // this is O(n).
 
+class Solutionn {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       vector<int> ans;
+        unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++){
+            if(mp.find(target - nums[i]) != mp.end()){
+                ans.push_back(mp[target - nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+            mp[nums[i]] = i;
+        }
+        return ans;
+    }
+};
+
 /*https://leetcode.com/problems/two-sum */
 
 int main() {
