917db549d72520fd582cb4bb3f50c1cd6476ac7f
Shyamal <sgprajapati24@gmail.com>
2021-11-17 23:09:13 +0530

    CC Starters 17: A

---
+++ codechef/GCD_of_Prefixes.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector> 
+using namespace std;
+
+#define int long long 
+#define nl "\n"
+const int M=1e9+7;
+typedef vector<int> vi;
+#define f(i,a,n) for(int i=a;i<n;i++)
+#define rf(i,a,n) for(int i=n-1;i>=a;i--)
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cin.tie(0);cout.tie(0);
+#define read(arr,n) for(int i=0;i<n;i++)cin>>arr[i];
+#define pr(arr,n) for(int i=0;i<n;i++)cout<<arr[i];
+#define in cin>>
+#define out cout<<
+#define first fr
+#define second sc
+
+void psi() 
+{
+
+int n; cin>>n;
+vi b,a; int x; bool c=true;
+f(i,0,n)
+{
+    cin>>x; 
+    b.push_back(x);
+    if(i>0 && b[i-1]%b[i]!=0)
+    {
+        c=false;
+    }
+}
+if(c)
+{
+    f(i,0,n)
+     cout << b[i] << ' ';
+    cout<<nl;
+}
+else cout << "-1\n";
+
+}
+
+int32_t main(){
+    FIO int t; t=1;
+    in t;
+    while(t--) psi();
+  
+return 0;
+}
