df18712f88e8ebcc17df96f111ec1564f212c987
Shyamal <sgprajapati24@gmail.com>
2022-01-03 11:12:03 +0530

    997 Town judge: Directed Graph

--- leetcodePractice/Graph/997FindTownJudge.cpp
+++ leetcodePractice/Graph/997FindTownJudge.cpp
@@ -1,28 +1,45 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        
-        if(trust.empty() && n==1)
-            return 1;
-
-        //for counting out degree of node
-        vector<int> out(n+1, 0);
-        
-        //for counting in degree of node
         vector<int> in(n+1, 0);
+        vector<int> out(n+1, 0);
+       /*
+        1.The town judge trusts nobody.
+        2.Everybody (except for the town judge) trusts the town judge.
+        3.There is exactly one person that satisfies properties 1 and 2.*/ 
+        if(trust.empty() && n == 1)
+            return 1;
         
-        //counts in-out degree 
+        /*this is the standard directed graph question, the town */
         for(auto edge: trust){
-            out[edge[0]]++;
+            out[edge[0]] ++;
             in[edge[1]]++;
         }
         
-        //if node has outdegree==0 means node doesnot trust anyone
-        //if indegree==n-1 means all other nodes trust this node except itself.
-        for(int i=0;i<=n;i++){
-            if(out[i]==0 && in[i]==n-1)
+        for(int i = 0; i <= n; i++){
+            if(out[i] == 0 && in[i] == n-1)
                 return i;
         }
+        
         return -1;
     }
 };
+
+int main(){
+    Solution ss;
+    vector<vector<int>> trust = {{1,2},{1,3},{2,3}};
+    cout<<ss.findJudge(3, trust);
+    return 0;
+}
+
+/*
+Vector:
+int ishani ={1,2,3,4,5,6,7,8,9,10};
+vector<int> v={1,2,3,4,5,6,7,8,9,10};
+vector<int> ishani;
+int arr[a][b];
+vector<vector<int>> vv;
+*/
