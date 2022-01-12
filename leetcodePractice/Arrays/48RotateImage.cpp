b96321040289c080d542b89a13a20bb60bd2e4ad
Shyamal <sgprajapati24@gmail.com>
2022-01-12 16:31:22 +0530

    48:Rotate Img

--- leetcodePractice/Arrays/48RotateImage.cpp
+++ leetcodePractice/Arrays/48RotateImage.cpp
@@ -1,16 +1,29 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+// https://leetcode.com/problems/rotate-image/
 class Solution {
- public:
-  void rotate(vector<vector<int>>& matrix) {
-    // as we can't use another 3D matrix for rotation,
-    // we directly jump to optimal solution- in place
-
-    int n = matrix.size();  // its m*m matrix.
-    for (int i = 0; i < n; i++)
-      for (int j = 0; j < i; j++)
-        swap(matrix[i][j], matrix[j][i]);
-
-    for (int i = 0; i < n; i++)
-      reverse(matrix[i].begin(), matrix[i].end());
-  }
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+       for(int i = 0; i < matrix.size(); i++){
+           for(int j = 0; j < i; j++)
+               swap(matrix[i][j], matrix[j][i]);
+       }
+        
+        for(int i = 0; i < matrix.size(); i++)
+            reverse(matrix[i].begin(),matrix[i].end());
+    }
 };
-// Matrix problem but didn't want to make another folder for it.
+
+int main(){
+    Solution s;
+    vector<vector<int>> matrix = {{1,2,3},{4,5,6},{7,8,9}};
+    s.rotate(matrix);
+    for(int i = 0; i < matrix.size(); i++){
+        for(int j = 0; j < matrix[i].size(); j++)
+            cout<<matrix[i][j]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
