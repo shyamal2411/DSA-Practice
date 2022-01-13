c3f7ad35144be50bf0e2247d6277f0d795af1f88
Shyamal <sgprajapati24@gmail.com>
2022-01-13 16:44:35 +0530

    452: number of balloons

--- leetcodePractice/Greedy/452MinNumberofBaloons.cpp
+++ leetcodePractice/Greedy/452MinNumberofBaloons.cpp
@@ -33,9 +33,66 @@ public:
 
 };
 
+//Better solution
+class Solutionn {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        sort(points.begin(), points.end());
+        
+        int start = points[0][0]; // start of first balloon  
+        int end = points[0][1]; // end of first balloon
+        
+        int ans = 1; //  we need atleast 1 arrow to burst first balloon
+        
+        //for other balloons
+        for(int i = 1; i < points.size(); i++){
+            if(points[i][0] <= end){ //if start of any balloon is before the end of a balloon, means they coincide
+                start = max(start, points[i][0]); //find the start value more to the right
+                end = min(end, points[i][1]); // find end value more to the left side for intersection
+            }
+            else{
+                //else we know that current balloon doesn't coincide
+                //so we set start and end to the current balloon and increase ans by one
+                start = points[i][0];
+                end = points[i][1];
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
+
 int main(){
     Solution ss;
     vector<vector<int>> points = {{10, 16}, {2, 8}, {1, 6}, {7,12}};
     cout<<ss.findMinArrowShots(points);
     return 0;
 }
+
+/*
+Case 1. Interescting overlapping intervals:
+    I--------I
+          I------I     
+--------------------
+>>        I--I
+We update the ans.back() with intersecting area since an array here would hit them both.
+
+
+Case 2. Intersecting included intervals:
+    I--------I
+       I--I
+------------------
+>>     I--I
+We update the ans.back() with intersecting area since an array here would hit them both.
+
+
+Case 3. Nonintersecting intervals:
+    I--------I
+                I--------I
+ ------------------------------
+>>  I--------I  I--------I
+We push another interval in ans() since there needs to be 2 arrows now to hit them both.
+
+There will never be a 4th case that the start of j is lesser than ans.back() since they are sorted.
+
+*/
