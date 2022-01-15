b152862c5c2159da3273272a1074ad35d378bd60
Shyamal <sgprajapati24@gmail.com>
2022-01-15 21:58:47 +0530

    Love babbar sheet restart

---
+++ bhagwan_question/Array_1/01_ReverseArr.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+// Love babbar sheet-1
+// 15/01/22
+// Reverse array
+// https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
+#define int long long 
+#define nl "\n"
+const int M=1e5 + 5;
+typedef vector<int> vi;
+#define f(i,a,n) for(int i=a;i<n;i++)
+#define rf(i,a,n) for(int i=n-1;i>=a;i--)
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cin.tie(0);cout.tie(0);
+#define read(arr,n) for(int i=0;i<n;i++)cin>>arr[i];
+#define pr(arr,n) for(int i=0;i<n;i++)cout<<arr[i];
+#define in cin>>
+#define out cout<<
+#define first fr
+#define second sc
+
+void rev(vector<int> &arr, int i, int j){
+    while(i < j){
+        swap(arr[i], arr[j]);
+        i++, j--;
+    }
+}
+
+void printt(vector<int> &arr, int size){
+    for(int i = 0; i < size; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+void solve(){
+ int size;
+ cin>>size;
+    vector<int> arr(size);
+    read(arr, size);
+    rev(arr, 0, size-1);
+    printt(arr, size);
+}
+
+int32_t main(){
+    FIO int t; t=1;
+    //in t;
+    while(t--) solve();
+  
+return 0;
+}
---
+++ bhagwan_question/Array_1/02_MinMaxArr.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+// Love babbar sheet-1
+// 15/01/22
+// Min max in arr
+// https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
+#define int long long 
+#define nl "\n"
+const int M=1e5 + 5;
+typedef vector<int> vi;
+#define f(i,a,n) for(int i=a;i<n;i++)
+#define rf(i,a,n) for(int i=n-1;i>=a;i--)
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cin.tie(0);cout.tie(0);
+#define read(arr,n) for(int i=0;i<n;i++)cin>>arr[i];
+#define pr(arr,n) for(int i=0;i<n;i++)cout<<arr[i];
+#define in cin>>
+#define out cout<<
+#define first fr
+#define second sc
+
+void rev(vector<int> &arr, int i, int j){
+    while(i < j){
+        swap(arr[i], arr[j]);
+        i++, j--;
+    }
+}
+
+void printt(vector<int> &arr, int size){
+    for(int i = 0; i < size; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+void minn(vector<int> &arr, int size){
+    int minEle = arr[0];
+    for(int i = 0; i < size; i++){
+        if(arr[i] < minEle)
+        minEle = arr[i];
+    }
+    cout<<"Min element: "<<minEle<<endl; 
+}
+
+void maxx(vector<int> &arr, int size){
+    int maxEle = arr[0];
+    for(int i = 0; i < size; i++){
+        if(arr[i] > maxEle)
+        maxEle= arr[i];
+    }
+    cout<<"Max element: "<<maxEle<<endl; 
+}
+
+
+void solve(){
+ int size;
+ cin>>size;
+    vector<int> arr(size);
+    read(arr, size);
+    minn(arr, size);
+    maxx(arr, size);
+    // rev(arr, 0, size-1);
+    // printt(arr, size);
+}
+
+int32_t main(){
+    FIO int t; t=1;
+    //in t;
+    while(t--) solve();
+  
+return 0;
+}
