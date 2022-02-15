7c2014fe0038254d19a5b67b30449878ad68a0bf
Shyamal <sgprajapati24@gmail.com>
2022-02-15 13:44:25 +0530

    Find Single Element

--- StriverSheet/BinarySearch/3_FindSingleElement.cpp
+++ StriverSheet/BinarySearch/3_FindSingleElement.cpp
@@ -19,11 +19,35 @@ public:
         }
         return ans;
     }
+    // BINARY SEARCH APPROACH - TAKEUFORWARD
+    int findSingleElement(vector<int> &nums){
+        int n = nums.size();
+        int low = 0;
+        int high = n-2;
+
+        while(low<=high){
+            int mid = (low + high) / 2;
+            if(mid%2 == 0){
+                if(nums[mid] != nums[mid+1])
+                high = mid - 1;
+                else
+                low = mid + 1;
+            } else
+            {
+                if(nums[mid] == nums[mid+1])
+                high = mid - 1;
+                else
+                low = mid + 1;
+            }
+        }
+        return nums[low];
+    }
 };
 
 int main(){
     Solution ss;
     vector<int> nums = {1,1,2,3,3,4,4,8,8};
-    cout<<ss.permuteUnique(nums);
+    // cout<<ss.permuteUnique(nums);
+    cout<<ss.findSingleElement(nums);
     return 0;
 }
