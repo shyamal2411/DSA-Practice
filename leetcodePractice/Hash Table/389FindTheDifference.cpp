7057aedf78412b3df20705a5fbf27c3dbbfbf543
Shyamal <sgprajapati24@gmail.com>
2022-02-24 21:32:28 +0530

    389: First YT Video

--- leetcodePractice/Hash Table/389FindTheDifference.cpp
+++ leetcodePractice/Hash Table/389FindTheDifference.cpp
@@ -1,69 +1,80 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h> 
 using namespace std;
 
-#define int long long
-#define nl "\n"
-const int M = 1e9 + 7;
-typedef vector<int> vi;
-typedef vector<string> vs;
-#define f(i, a, n) for (int i = a; i < n; i++)
-#define FIO                         \
-  ios_base::sync_with_stdio(false); \
-  cin.tie(NULL);                    \
-  cin.tie(0);                       \
-  cout.tie(0);
-#define read(arr, n)          \
-  for (int i = 0; i < n; i++) \
-    cin >> arr[i];
-#define pr(arr, n)            \
-  for (int i = 0; i < n; i++) \
-    cout << arr[i] << " ";
-#define in cin >>
-#define out cout <<
-
+// https://leetcode.com/problems/find-the-difference/
 class Solution {
- public:
-  char findTheDifference(string s, string t) {
-    unordered_map<char, int> sh, th;
-    // take 2 maps and insert the characters of s in sh
-    char result = ' ';  // function type is char, so for returning char
+public:
+    char findTheDifference(string s, string t) {
+        
+//         Approach-1 using hashmap
+         map<char, int> mp;
+        char c;
+        for(auto it: s){
+            mp[it]++;
+        }
+        for(auto it: t){
+            mp[it]++;
+        }
+       for(auto i: mp){
+            if(i.second %2 != 0)
+                return i.first;
+        }
+        return 'a';
+        
+//         TC: O(n)
+//         SC: O(n)
+        
+        
+// --------------------------------------------------------
+//         Approach-2 sorting elements
+        
+		// sort(s.begin(), s.end());
+		// sort(t.begin(), t.end());
+		// int i = 0;
+		// while(i < s.size()){
+		// 	if(s[i] != t[i]){
+		// 		return t[i];
+		// 	}
+		// 	i++;
+		// }
+		// return t[s.size()];
 
-    for (char ch : s)
-      sh[ch]++;  // inserting all the characters of s into map-sh
+//         TC: O(n*logn)
+//         SC: O(n) 
 
-    for (char ch : t) {
-      th[ch]++;  // insert characters & check if the char value of th more than
-                 // sh i.e. check if the char is out of s string's out of the
-                 // area or not
-      if (th[ch] > sh[ch]) {
-        result = ch;  // if it is then assign it to result char & break
-        break;
-      }
+// --------------------------------------------------------
+
+//         Appcoach-3 Sum Diff
+		// int sum1 = 0;
+		// int sum2 = t[s.size()];
+		// for(int i = 0; i < s.length(); i++){
+		// 	sum1 += s[i];
+		// 	sum2 += t[i];
+		// }
+		// return char(abs(sum2 - sum1));
+
+        // TC: O(n)
+//         SC: O(1)
+        
+// --------------------------------------------------------
+
+//         Approach-4 XOR Application
+		// char res = t[s.size()];
+		// for(int i = 0; i < s.size(); i++){
+		// 	res ^= s[i] ^ t[i];
+		// }
+		// return res;
+        
+//         TC: O(n)
+//         SC: O(1)
+        
     }
-    return result;
-  }
 };
-void solve() {
-  int n, k;
-  string s, t;
-  cin >> s >> t;
+
+int main(){
   Solution ss;
-  //   ss.findTheDifference(s,t);
-  cout << ss.findTheDifference(s, t) << nl;
-}
-
-int32_t main() {
-  FIO int t;
-  t = 1;
-  //   in t;
-  while (t--)
-    solve();
-
+  string s = "abcd";
+  string t = "abcde";
+  cout<<ss.findTheDifference(s, t)<<endl;
   return 0;
 }
-/*
-INPUT:
-abcd abcde
-OUTPUT:
-e
-*/
