3a0f36938dd2dbfe204079192447f5534333c32c
Shyamal <sgprajapati24@gmail.com>
2022-02-25 16:04:14 +0530

    165: 2nd video

--- leetcodePractice/2Pointer/165CompareVersions.cpp
+++ leetcodePractice/2Pointer/165CompareVersions.cpp
@@ -7,26 +7,26 @@ class Solution
 public:
     int compareVersion(string version1, string version2)
     {
-        int i = 0, j = 0, nn = 0, mm = 0;
+        int i = 0, j = 0, sum1 = 0, sum2 = 0;
         int n = version1.size(), m = version2.size();
         while (i < n || j < m)
         {
             while (i < n && version1[i] != '.')
             {
-                nn = nn * 10 + (version1[i] - '0');
+                sum1 = sum1 * 10 + (version1[i] - '0');
                 i++;
             }
             while (j < m && version2[j] != '.')
             {
-                mm = mm * 10 + (version2[j] - '0');
+                sum2 = sum2 * 10 + (version2[j] - '0');
                 j++;
             }
-            if (nn > mm)
+            if (sum1 > sum2)
                 return 1;
-            else if(nn < mm)
+            else if(sum1 < sum2)
                 return -1;
 
-            nn = 0, mm = 0;
+            sum1 = 0, sum2 = 0;
             i++, j++;
         }
         return 0;
