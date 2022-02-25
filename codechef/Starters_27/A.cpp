c80fa717a1ffc4c0e1b86710f0d020b04a6e7169
Shyamal <sgprajapati24@gmail.com>
2022-02-25 15:06:42 +0530

    CC Starters 27

---
+++ codechef/Starters_27/A.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+#define int long long 
+#define nl "\n"
+const int M=1e5 + 5;
+typedef vector<int> vi;
+#define f(i,a,n) for(int i=a;i<n;i++)
+#define rf(i,a,n) for(int i=n-1;i>=a;i--)
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cin.tie(0);cout.tie(0);
+#define read(arr,n) for(int i=0;i<n;i++)cin>>arr[i];
+#define pr(arr,n) for(int i=0;i<n;i++)cout<<arr[i];
+#define in cin>>
+#define out cout<<
+#define first fr
+#define second sc
+
+void solve(){
+ int a, b; cin>>a>>b;
+    int ans = 0;
+    ans = 21 - (a + b);
+    if(ans > 10)
+    cout<< -1<<endl;
+    else
+    cout<<ans<<endl;
+}
+
+
+int32_t main(){
+    FIO int t; t=1;
+    in t;
+    while(t--) solve();
+  
+return 0;
+}
