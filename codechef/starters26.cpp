d643ce47d9aef1385ce7d72bada51c5994a29de3
Shyamal <sgprajapati24@gmail.com>
2022-03-06 21:40:58 +0530

    starters 26

---
+++ codechef/starters26.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+#define int long long 
+#define nl "\n"
+const int M=1e5 + 5;
+typedef vector<int> vi;
+#define f(i,a,n) for(int i=a;i<n;i++)
+#define rf(i,a,n) for(int i=n-1;i>=a;i--)
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cin.tie(0);cout.tie(0);
+#define read(arr,n) for(int i=0;i<n;i++)cin>>arr[i];
+#define pr(arr,n) for(int i=0;i<n;i++)cout<<arr[i];
+#define in cin>>
+#define out cout<<
+#define first fr
+#define second sc
+
+void solve(){
+ int x, y;
+ cin>>x>>y;
+ if(x>y) cout<<"BIKE\n";
+ else if(x<y) cout<<"CAR\n";
+ else cout<<"SAME\n";
+}
+
+
+int32_t main(){
+    FIO int t; t=1;
+    in t;
+    while(t--) solve();
+  
+return 0;
+}
