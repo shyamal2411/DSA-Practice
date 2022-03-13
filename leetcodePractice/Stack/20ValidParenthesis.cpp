640a9cd6b60e1ece9b53391876f66f34d5adf559
Shyamal <sgprajapati24@gmail.com>
2022-03-13 18:24:02 +0530

    Valid Parenthesis

--- leetcodePractice/Stack/20ValidParenthesis.cpp
+++ leetcodePractice/Stack/20ValidParenthesis.cpp
@@ -1,18 +1,7 @@
 #include<bits/stdc++.h> 
 using namespace std;
 
-#define int long long 
-#define nl "\n"
-const int M=1e9+7;
-typedef vector<int> vi;
-typedef vector<string> vs;
-#define f(i,a,n) for(int i=a;i<n;i++)
-#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cin.tie(0);cout.tie(0);
-#define read(arr,n) for(int i=0;i<n;i++)cin>>arr[i];
-#define pr(arr,n) for(int i=0;i<n;i++)cout<<arr[i]<<" ";
-#define in cin>>
-#define out cout<<
-
+// https://leetcode.com/problems/valid-parentheses/
 class Solution {
  public:
   bool isValid(string s) {
@@ -36,21 +25,10 @@ class Solution {
   }
 };
 
-void solve(){
-int n,k;
-string s;
-cin>>s;
- Solution ss;
- if (ss.isValid(s))
-   cout << "true\n";
- else
-   cout << "false\n";
-}
-
-int32_t main(){
-    FIO int t; t=1;
-    in t;
-    while(t--) solve();
-  
-return 0;
+int main(){
+    Solution ss;
+    string s;
+    cin>>s;
+    cout<<ss.isValid(s);
+    return 0;
 }
