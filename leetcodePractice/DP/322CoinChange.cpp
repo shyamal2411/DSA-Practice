4e3862c7121b34c7511cdb9bd435085ac44a7b28
SG <sgprajapati24@gmail.com>
2022-05-21 10:17:07 +0530

    Coin Change Tabulation method

--- leetcodePractice/DP/322CoinChange.cpp
+++ leetcodePractice/DP/322CoinChange.cpp
@@ -26,6 +26,37 @@ public:
     }
 };
 
+class Solution {
+public:
+    int dp[10000 + 1][12 + 1];
+    
+    int tabulation(vector<int> wt, int w, int n)
+    {
+        for (int i = 0; i < w + 1; i++)
+            for (int j = 0; j < n + 1; j++)
+                if (i == 0 || j == 0)
+                    dp[i][j] = (i == 0) ? 0 : INT_MAX - 1;
+        
+        for (int i = 1; i < w + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (wt[j - 1] > i) 
+                    dp[i][j] = 0 + dp[i - 0][j - 1];
+                else 
+                    dp[i][j] = min(0 + dp[i - 0][j - 1], 1 + dp[i - wt[j - 1]][j - 0]);
+            }
+        }
+        
+        return dp[w][n];
+    }
+    
+    int coinChange(vector<int>& coins, int amount) 
+    {
+        memset(dp, -1, sizeof(dp));
+        int minCoins = tabulation(coins, amount, coins.size());
+        return minCoins == INT_MAX - 1 ? -1 : minCoins;    
+    }
+};
+
 int main(){
     Solution ss;
     vector<int> s={1,2,5};
