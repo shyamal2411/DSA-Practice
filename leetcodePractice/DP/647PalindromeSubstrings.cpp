4d557bf955d84b73170a90787398914c63c9c9c4
SG <sgprajapati24@gmail.com>
2022-05-22 20:11:58 +0530

    Tabulation method

--- leetcodePractice/DP/647PalindromeSubstrings.cpp
+++ leetcodePractice/DP/647PalindromeSubstrings.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 // https://leetcode.com/problems/palindromic-substrings/
+// Tabulation method: https://leetcode.com/problems/palindromic-substrings/discuss/631588/C%2B%2B-DP-with-video-link
 class Solution {
 public:
     int count = 0;
@@ -24,6 +25,32 @@ public:
         findSubstrings(s, i, s.size());
         }
         
+        return count;
+    }
+};
+
+// Tabulation method
+class Solutionn {
+public:
+    int countSubstrings(string s) {
+        int count = 0;
+        int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n));
+        for(int i = 0;i<n;i++) {
+            dp[i][i] = 1;
+            count++;
+        }
+        for(int col = 1;col<n;col++) {
+            for(int row = 0;row < col;row++) {
+                if(row ==col - 1 && s[row] == s[col]) {
+                    dp[row][col] = 1;
+                    count++;
+                } else if(s[row] == s[col] && dp[row + 1][col-1] == 1) {
+                    dp[row][col] = 1;
+                    count++;
+                }
+            }
+        }
         return count;
     }
 };
