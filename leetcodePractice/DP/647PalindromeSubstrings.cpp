412f5a0b3e2508aa711bae911ace6709d9948a89
SG <sgprajapati24@gmail.com>
2022-05-22 19:59:51 +0530

    Code test

---
+++ leetcodePractice/DP/647PalindromeSubstrings.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+
+class Solution {
+public:
+    int count = 0;
+    void findSubstrings(string s, int start, int n){
+        for(int end = start; end < n; end++){
+            if(isPalindrome(start, end, s)){count++;}
+        }
+    }
+    
+    bool isPalindrome(int start, int end, string s){
+        while(start <= end){
+            if(s[start++] != s[end--]){
+                return false;}
+        }
+        return false;
+    }
+    
+    int countSubstrings(string s) {
+    for(int i = 0; i < s.size(); i++){
+        findSubstrings(s, i, s.size());
+        }
+        
+        return count;
+    }
+};
+
+int main(){
+    Solution s;
+    string str = "aaa";
+    cout<<s.countSubstrings(str);
+    return 0;
+}
