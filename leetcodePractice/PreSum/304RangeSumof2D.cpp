1bb767c10ea258571f081f62fd4b084da99adf9d
SG <sgprajapati24@gmail.com>
2022-06-03 18:06:28 +0530

    304: PreSum on 2D matrix

---
+++ leetcodePractice/PreSum/304RangeSumof2D.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+// https://leetcode.com/problems/range-sum-query-2d-immutable/
+class NumMatrix {
+public:
+    vector<vector<int>> sum;
+    NumMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        sum = vector<vector<int>>(m + 1, vector<int>(n + 1)); // sum[i][j] is sum of all elements inside the rectangle [0,0,i,j]
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + matrix[i - 1][j - 1];
+            }
+        }
+    }
+    int sumRegion(int r1, int c1, int r2, int c2) {
+		// Since our `sum` starts by 1 so we need to increase r1, c1, r2, c2 by 1
+        r1++; c1++; r2++; c2++; 
+        return sum[r2][c2] - sum[r2][c1 - 1] - sum[r1 - 1][c2] + sum[r1 - 1][c1 - 1];
+    }
+};
+
+int main(){
+
+    // NumMatrix nm; 
+    // NumMatrix* obj = new NumMatrix(matrix);
+    vector<vector<int>>{{3,0,1,4,2},{5,6,3,2,1},{1,2,0,1,5},{4,1,0,1,7},{1,0,3,0,5}};
+    // int param_1 = obj->sumRegion(row1,col1,row2,col2);
+    // cout << nm.sumRegion(2,1,4,3) << endl;
+    return 0;
+}
