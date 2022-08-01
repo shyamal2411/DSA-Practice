b4f402676299f95a0bc857ad21884f237ce681ac
SG <sgprajapati24@gmail.com>
2022-08-01 12:00:14 +0530

    Invert Binary Tree- Level order Traversal

--- StriverSheet/Tree/invertBinaryTree.cpp
+++ StriverSheet/Tree/invertBinaryTree.cpp
@@ -1,3 +1,4 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -14,30 +15,29 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution
 {
 public:
+    //     TreeNode* invertTree(TreeNode* root) {
+    //          if(!root)
+    //             return NULL;
 
-  TreeNode* invertTree2(TreeNode* root) {
-    if (!root)
-      return NULL;
-    TreeNode* node = root->right;
-    root->right = invertTree2(root->left);
-    root->left = invertTree2(node);
-
-    return root;
-  }
-
-// BOTH WAY WORKS - EITHER TAKE NODE AS LEFT OR RIGHT
-
-    TreeNode* invertTree3(TreeNode* root) {
-        if(!root) return NULL;
-        TreeNode* node=root->left;
-        root->left=invertTree(root->right);
-        root->right=invertTree(node);
-
-        return root;
-    }
+    //         TreeNode* node=root->left;
+    //         root->left=invertTree(root->right);
+    //         root->right=invertTree(node);
+    //         return root;
+    //     }
 
     TreeNode *invertTree(TreeNode *root)
     {
@@ -71,15 +71,6 @@ public:
 int main()
 {
     Solution ss;
-    TreeNode *root = new TreeNode(1);
-    root->left = new TreeNode(2);
-    root->right = new TreeNode(3);
-    root->left->left = new TreeNode(4);
-    root->left->right = new TreeNode(5);
-    root->right->left = new TreeNode(6);
-    root->right->right = new TreeNode(7);
-
-    ss.invertTree(root);
-
+    vector<int> vec = {};
     return 0;
 }
