fc10593e554a10e619e78bcbf9cf05753aec0814
SG <sgprajapati24@gmail.com>
2022-11-07 23:59:27 +0530

    Amazon OA question

---
+++ Code2.0/OA/AmazonOA_Alexa.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+#define int long long 
+#define nl "\n"
+const int M=1e5 + 5;
+typedef vector<int> vi;
+#define f(i,a,n) for(int i=a;i<n;i++)
+#define rf(i,a,n) for(int i=n-1;i>=a;i--)
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cin.tie(0);cout.tie(0);
+#define read(arr,n) for(int i=0;i<n;i++)cin>>arr[i];
+#define pr(arr,n) for(int i=0;i<n;i++)cout<<arr[i];
+#define in cin>>
+#define out cout<<
+#define umap unordered_map<int, int> mp
+#define input int n; cin>>n;
+#define inmap for(auto i: mp) mp[i]++;
+
+void solve(){
+ 
+}
+
+
+long tempchange(vi temp){
+    int n = temp.size();
+    
+}
+
+int32_t main(){
+    FIO int t; t=1;
+    //in t;
+    while(t--) solve();
+  
+return 0;
+}
